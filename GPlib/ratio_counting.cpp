fbfa5137bb5695b640b5c22491934709ef7df4e1
CondensedOtters <moog.mathieu@gmail.com>
2017-11-22 00:18:21 +0100

    modified:   ratio_counting.cpp

--- GPlib/ratio_counting.cpp
+++ GPlib/ratio_counting.cpp
@@ -32,16 +32,10 @@
 int main( void )
 {
   //--------
-  // Input
-  //---------------------------------
-  std::ifstream input("TRAJEC.xyz");
-  //---------------------------------
-
-  //--------
   // Output
   //--------------------------------------------------------------------------------
-  std::ofstream output3CO_1st( "3C_1st.dat" ,  std::ios::out );
-  std::ofstream output3CO_2nd( "3C_2nd.dat" ,  std::ios::out );
+  std::ofstream output4CO_1st( "4CO_1st.dat" ,  std::ios::out );
+  std::ofstream output4CO_2nd( "4CO_2nd.dat" ,  std::ios::out );
   //--------------------------------------------------------------------------------
   
   //------
@@ -56,20 +50,20 @@ int main( void )
   // 3CO
   //------------------------------------------------
   // 2500K
-  hist_list.push_back( readRegularHistogramReal( "3500K/40GPa/3nearestCO.dat" ) );
-  hist_list.push_back( readRegularHistogramReal( "2500K/45GPa/3nearestCO.dat" ) );
-  hist_list.push_back( readRegularHistogramReal( "2500K/50GPa/3nearestCO.dat" ) );
-  hist_list.push_back( readRegularHistogramReal( "2500K/60GPa/3nearestCO.dat" ) );
+  hist_list.push_back( readRegularHistogramReal( "3500K/40GPa/4nearestCO.dat" ) );
+  hist_list.push_back( readRegularHistogramReal( "2500K/45GPa/4nearestCO.dat" ) );
+  hist_list.push_back( readRegularHistogramReal( "2500K/50GPa/4nearestCO.dat" ) );
+  hist_list.push_back( readRegularHistogramReal( "2500K/60GPa/4nearestCO.dat" ) );
   // 3000K
-  hist_list.push_back( readRegularHistogramReal( "3000K/40GPa/3nearestCO.dat" ) );
-  hist_list.push_back( readRegularHistogramReal( "3000K/45GPa/3nearestCO.dat" ) );
-  hist_list.push_back( readRegularHistogramReal( "3000K/50GPa/3nearestCO.dat" ) );
-  hist_list.push_back( readRegularHistogramReal( "3000K/60GPa/3nearestCO.dat" ) );
+  hist_list.push_back( readRegularHistogramReal( "3000K/40GPa/4nearestCO.dat" ) );
+  hist_list.push_back( readRegularHistogramReal( "3000K/45GPa/4nearestCO.dat" ) );
+  hist_list.push_back( readRegularHistogramReal( "3000K/50GPa/4nearestCO.dat" ) );
+  hist_list.push_back( readRegularHistogramReal( "3000K/60GPa/4nearestCO.dat" ) );
   // 3500K
-  hist_list.push_back( readRegularHistogramReal( "3500K/40GPa/3nearestCO.dat" ) );
-  hist_list.push_back( readRegularHistogramReal( "3500K/45GPa/3nearestCO.dat" ) );
-  hist_list.push_back( readRegularHistogramReal( "3500K/50GPa/3nearestCO.dat" ) );
-  hist_list.push_back( readRegularHistogramReal( "3500K/60GPa/3nearestCO.dat" ) );
+  hist_list.push_back( readRegularHistogramReal( "3500K/40GPa/4nearestCO.dat" ) );
+  hist_list.push_back( readRegularHistogramReal( "3500K/45GPa/4nearestCO.dat" ) );
+  hist_list.push_back( readRegularHistogramReal( "3500K/50GPa/4nearestCO.dat" ) );
+  hist_list.push_back( readRegularHistogramReal( "3500K/60GPa/4nearestCO.dat" ) );
   //-------------------------------------------------------------------i
 
   //----------
@@ -83,17 +77,16 @@ int main( void )
   //-------------------------------------------------------------------
   for ( int i=0 ; i < 4 ; i++ )
     {
-      output3CO_1st << press[i] << " " << integrateHistogram( hist_list[i] , 0 , 1.75 ) << " " << integrateHistogram( hist_list[i+4] , 0 , 1.75 ) << " " << integrateHistogram( hist_list[i+8] , 0 , 1.75 )  <<  std::endl;
-      output3CO_2nd << press[i] << " " << integrateHistogram( hist_list[i] , 1.75 , 3.00 ) << " " << integrateHistogram( hist_list[i+4] , 1.75 , 3.00 ) << " " << integrateHistogram( hist_list[i+8] , 1.75 , 3.00 )  <<  std::endl;
+      output4CO_1st << press[i] << " " << integrateHistogram( hist_list[i] , 0 , 1.75 ) << " " << integrateHistogram( hist_list[i+4] , 0 , 1.75 ) << " " << integrateHistogram( hist_list[i+8] , 0 , 1.75 )  <<  std::endl;
+      output4CO_2nd << press[i] << " " << integrateHistogram( hist_list[i] , 1.75 , 3.00 ) << " " << integrateHistogram( hist_list[i+4] , 1.75 , 3.00 ) << " " << integrateHistogram( hist_list[i+8] , 1.75 , 3.00 )  <<  std::endl;
     }
   //-------------------------------------------------------------------
   
   //--------------
   //Closing fluxes
   //----------------------
-  input.close();
-  output3CO_1st.close();
-  output3CO_2nd.close();
+  output4CO_1st.close();
+  output4CO_2nd.close();
   //----------------------
   
   return 0;
