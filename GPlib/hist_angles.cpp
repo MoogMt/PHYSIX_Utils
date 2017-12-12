5098b70a991ccc9dc6ab50f0abf0648f799d0cfc
CondensedOtters <moog.mathieu@gmail.com>
2017-12-12 15:07:52 +0100

    modified:   hist_angles.cpp 	modified:   makefile

--- GPlib/hist_angles.cpp
+++ GPlib/hist_angles.cpp
@@ -50,54 +50,56 @@ int main( void )
   //---------------------------------------------------------------------------------------------
   // CO2
   //---------------------------------------------------------------------------------------------
-  std::vector<BinReal> angles2_882_2000_hist = readRegularHistogramReal("8.82/2000K/pressure.dat");
-  std::vector<BinReal> angles2_900_2000_hist = readRegularHistogramReal("9.0/2000K/pressure.dat");
-  std::vector<BinReal> angles2_980_2000_hist = readRegularHistogramReal("9.8/2000K/pressure.dat");
-  std::vector<BinReal> angles2_882_2500_hist = readRegularHistogramReal("8.82/2500K/pressure.dat");
-  std::vector<BinReal> angles2_900_2500_hist = readRegularHistogramReal("9.0/2500K/pressure.dat");
-  std::vector<BinReal> angles2_980_2500_hist = readRegularHistogramReal("9.8/2500K/pressure.dat");
-  std::vector<BinReal> angles2_882_3000_hist = readRegularHistogramReal("8.82/3000K/pressure.dat");
-  std::vector<BinReal> angles2_900_3000_hist = readRegularHistogramReal("9.0/3000K/pressure.dat");
-  std::vector<BinReal> angles2_980_3000_hist = readRegularHistogramReal("9.8/3000K/pressure.dat");
+  std::vector<BinReal> angles2_882_2000_hist = readRegularHistogramReal("8.82/2000K/c2angles.dat");
+  std::vector<BinReal> angles2_900_2000_hist = readRegularHistogramReal("9.0/2000K/c2angles.dat");
+  std::vector<BinReal> angles2_980_2000_hist = readRegularHistogramReal("9.8/2000K/c2angles.dat");
+  std::vector<BinReal> angles2_882_2500_hist = readRegularHistogramReal("8.82/2500K/c2angles.dat");
+  std::vector<BinReal> angles2_900_2500_hist = readRegularHistogramReal("9.0/2500K/c2angles.dat");
+  std::vector<BinReal> angles2_980_2500_hist = readRegularHistogramReal("9.8/2500K/c2angles.dat");
+  std::vector<BinReal> angles2_882_3000_hist = readRegularHistogramReal("8.82/3000K/c2angles.dat");
+  std::vector<BinReal> angles2_900_3000_hist = readRegularHistogramReal("9.0/3000K/c2angles.dat");
+  std::vector<BinReal> angles2_980_3000_hist = readRegularHistogramReal("9.8/3000K/c2angles.dat");
   //---------------------------------------------------------------------------------------------
   // CO3
   //---------------------------------------------------------------------------------------------
-  std::vector<BinReal> angles3_882_2000_hist = readRegularHistogramReal("8.82/2000K/pressure.dat");
-  std::vector<BinReal> angles3_900_2000_hist = readRegularHistogramReal("9.0/2000K/pressure.dat");
-  std::vector<BinReal> angles3_980_2000_hist = readRegularHistogramReal("9.8/2000K/pressure.dat");
-  std::vector<BinReal> angles3_882_2500_hist = readRegularHistogramReal("8.82/2500K/pressure.dat");
-  std::vector<BinReal> angles3_900_2500_hist = readRegularHistogramReal("9.0/2500K/pressure.dat");
-  std::vector<BinReal> angles3_980_2500_hist = readRegularHistogramReal("9.8/2500K/pressure.dat");
-  std::vector<BinReal> angles3_882_3000_hist = readRegularHistogramReal("8.82/3000K/pressure.dat");
-  std::vector<BinReal> angles3_900_3000_hist = readRegularHistogramReal("9.0/3000K/pressure.dat");
-  std::vector<BinReal> angles3_980_3000_hist = readRegularHistogramReal("9.8/3000K/pressure.dat");
+  std::vector<BinReal> angles3_882_2000_hist = readRegularHistogramReal("8.82/2000K/c3angles.dat");
+  std::vector<BinReal> angles3_900_2000_hist = readRegularHistogramReal("9.0/2000K/c3angles.dat");
+  std::vector<BinReal> angles3_882_2500_hist = readRegularHistogramReal("8.82/2500K/c3angles.dat");
+  std::vector<BinReal> angles3_900_2500_hist = readRegularHistogramReal("9.0/2500K/c3angles.dat");
+  std::vector<BinReal> angles3_882_3000_hist = readRegularHistogramReal("8.82/3000K/c3angles.dat");
+  std::vector<BinReal> angles3_900_3000_hist = readRegularHistogramReal("9.0/3000K/c3angles.dat");
   //---------------------------------------------------------------------------------------------
   // CO4
   //---------------------------------------------------------------------------------------------
-  std::vector<BinReal> angles4_882_2000_hist = readRegularHistogramReal("8.82/2000K/pressure.dat");
-  std::vector<BinReal> angles4_900_2000_hist = readRegularHistogramReal("9.0/2000K/pressure.dat");
-  std::vector<BinReal> angles4_980_2000_hist = readRegularHistogramReal("9.8/2000K/pressure.dat");
-  std::vector<BinReal> angles4_882_2500_hist = readRegularHistogramReal("8.82/2500K/pressure.dat");
-  std::vector<BinReal> angles4_900_2500_hist = readRegularHistogramReal("9.0/2500K/pressure.dat");
-  std::vector<BinReal> angles4_980_2500_hist = readRegularHistogramReal("9.8/2500K/pressure.dat");
-  std::vector<BinReal> angles4_882_3000_hist = readRegularHistogramReal("8.82/3000K/pressure.dat");
-  std::vector<BinReal> angles4_900_3000_hist = readRegularHistogramReal("9.0/3000K/pressure.dat");
-  std::vector<BinReal> angles4_980_3000_hist = readRegularHistogramReal("9.8/3000K/pressure.dat");
+  std::vector<BinReal> angles4_882_2000_hist = readRegularHistogramReal("8.82/2000K/c4angles.dat");
+  std::vector<BinReal> angles4_900_2000_hist = readRegularHistogramReal("9.0/2000K/c4angles.dat");
+  std::vector<BinReal> angles4_882_2500_hist = readRegularHistogramReal("8.82/2500K/c4angles.dat");
+  std::vector<BinReal> angles4_900_2500_hist = readRegularHistogramReal("9.0/2500K/c4angles.dat");
+  std::vector<BinReal> angles4_882_3000_hist = readRegularHistogramReal("8.82/3000K/c4angles.dat");
+  std::vector<BinReal> angles4_900_3000_hist = readRegularHistogramReal("9.0/3000K/c4angles.dat");
   //---------------------------------------------------------------------------------------------
 
   //------------------
   // Compute averages
   //---------------------
-  std::cout << "8.82 " << average(press_882_2000_hist) << " " << average(press_882_2500_hist) << " " << average(press_882_3000_hist) << " " << std::endl;
-  std::cout << "9.00 " << average(press_900_2000_hist) << " " << average(press_900_2500_hist) << " " << average(press_900_3000_hist) << " " << std::endl;
-  std::cout << "9.80 " << average(press_980_2000_hist) << " " << average(press_980_2500_hist) << " " << average(press_980_3000_hist) << " " << std::endl;
+  output2 << "8.82 " << average( angles2_882_2000_hist ) << " " << average( angles2_882_2500_hist ) << " " << average( angles2_882_3000_hist ) << " " << std::endl;
+  output2 << "9.00 " << average( angles2_900_2000_hist ) << " " << average( angles2_900_2500_hist ) << " " << average( angles2_900_3000_hist ) << " " << std::endl;
+  output2 << "9.80 " << average( angles2_980_2000_hist ) << " " << average( angles2_980_2500_hist ) << " " << average( angles2_980_3000_hist ) << " " << std::endl;
   //---------------------
+    output3 << "8.82 " << average( angles3_882_2000_hist ) << " " << average( angles3_882_2500_hist ) << " " << average( angles3_882_3000_hist ) << " " << std::endl;
+    output3 << "9.00 " << average( angles3_900_2000_hist ) << " " << average( angles3_900_2500_hist ) << " " << average( angles3_900_3000_hist ) << " " << std::endl;
+  //------------------------
+    output4 << "8.82 " << average( angles4_882_2000_hist ) << " " << average( angles4_882_2500_hist ) << " " << average( angles4_882_3000_hist ) << " " << std::endl;
+    output4 << "9.00 " << average( angles4_900_2000_hist ) << " " << average( angles4_900_2500_hist ) << " " << average( angles4_900_3000_hist ) << " " << std::endl;
+  //------------------------
     
   //--------------
   //Closing fluxes
   //----------------------
   input.close();
-  output.close();
+  output2.close();
+  output3.close();
+  output4.close();
   //----------------------
   
   return 0;
