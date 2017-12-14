cb9a57ec3471d3c4671e743591661793b140c8f3
CondensedOtters <moog.mathieu@gmail.com>
2017-12-14 12:45:33 +0100

    renamed:    NN.cpp -> CppLib/NN.cpp 	renamed:    angles.cpp -> CppLib/angles.cpp 	renamed:    atom.cpp -> CppLib/atom.cpp 	renamed:    atom.h -> CppLib/atom.h 	renamed:    bonds.cpp -> CppLib/bonds.cpp 	renamed:    bonds.h -> CppLib/bonds.h 	renamed:    c3c4.cpp -> CppLib/c3c4.cpp 	renamed:    cell.cpp -> CppLib/cell.cpp 	renamed:    cell.h -> CppLib/cell.h 	renamed:    compress.cpp -> CppLib/compress.cpp 	renamed:    computeHist.cpp -> CppLib/computeHist.cpp 	renamed:    computeSpeed.cpp -> CppLib/computeSpeed.cpp 	renamed:    contact_matrix.cpp -> CppLib/contact_matrix.cpp 	renamed:    contact_matrix.h -> CppLib/contact_matrix.h 	renamed:    cutoff.cpp -> CppLib/cutoff.cpp 	renamed:    cutoff.h -> CppLib/cutoff.h 	renamed:    diffusion.cpp -> CppLib/diffusion.cpp 	renamed:    geometry.cpp -> CppLib/geometry.cpp 	renamed:    geometry.h -> CppLib/geometry.h 	renamed:    hist_angles.cpp -> CppLib/hist_angles.cpp 	renamed:    histogram.cpp -> CppLib/histogram.cpp 	renamed:    histogram.h -> CppLib/histogram.h 	renamed:    lut.cpp -> CppLib/lut.cpp 	renamed:    lut.h -> CppLib/lut.h 	renamed:    make_molecules.cpp -> CppLib/make_molecules.cpp 	renamed:    makefile -> CppLib/makefile 	new file:   CppLib/mol_id.cpp 	renamed:    molecules.cpp -> CppLib/molecules.cpp 	renamed:    molecules.h -> CppLib/molecules.h 	renamed:    pdb.cpp -> CppLib/pdb.cpp 	renamed:    pdb.h -> CppLib/pdb.h 	renamed:    pressure.cpp -> CppLib/pressure.cpp 	renamed:    ratio_counting.cpp -> CppLib/ratio_counting.cpp 	renamed:    read.cpp -> CppLib/read.cpp 	renamed:    sim.cpp -> CppLib/sim.cpp 	renamed:    sim.h -> CppLib/sim.h 	renamed:    utils.cpp -> CppLib/utils.cpp 	renamed:    utils.h -> CppLib/utils.h 	renamed:    wrap.cpp -> CppLib/wrap.cpp 	renamed:    xyz.cpp -> CppLib/xyz.cpp 	renamed:    xyz.h -> CppLib/xyz.h 	new file:   PythonLib/ComputeSpeed.py 	new file:   PythonLib/computeCorrSZ.py

---
+++ GPlib/PythonLib/ComputeSpeed.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 14 12:42:30 2017
+
+@author: moog
+"""
+
---
+++ GPlib/PythonLib/computeCorrSZ.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 14 12:43:21 2017
+
+@author: moog
+"""
+
+rwkfds
