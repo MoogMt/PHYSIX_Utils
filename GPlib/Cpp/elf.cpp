25d920709bf4cb4e71099f9c01f4d283c87bad5e
CondensedOtters <moog.mathieu@gmail.com>
2018-03-02 19:32:37 +0100

    new file:   elf.cpp 	new file:   readcolvar.cpp 	new file:   ../Fortran/make_mols 	new file:   ../Julia/check.jl 	new file:   ../../Map/plot-scattered-simann.sh

---
+++ GPlib/Cpp/elf.cpp
@@ -0,0 +1,81 @@
+//---------------
+// External Libs
+//-------------------
+#include <fstream>
+#include <iterator>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stdio.h>  
+#include <stdlib.h>    
+#include <math.h>
+//---------------------
+
+//----------------
+// Internal Files
+//-------------------------
+#include "utils.h"
+#include "atom.h"
+#include "cell.h"
+#include "contact_matrix.h"
+#include "xyz.h"
+#include "histogram.h"
+#include "pdb.h"
+#include "sim.h"
+#include "lut.h"
+#include "molecules.h"
+//-------------------------
+
+//================
+// MAIN PROGRAM
+//=====================================================================
+int main( void )
+{
+  //--------
+  // Input
+  //---------------------------------
+  std::ifstream input("ELF.cube");
+  //--------------------------------
+
+  //--------
+  // Output
+  //-------------------------------------
+  std::ofstream output("output.dat");
+  //-------------------------------------
+  
+  //---------------
+  // Initializers
+  //-------------------------------------
+  AtomList  atom_list; // Atoms in cell
+  AllTypeLUT lut_list; // LUT for types
+  ContactMatrix    cm; // Contact matrix
+  //-------------------------------------
+
+  //--------------------
+  // Reading Cell File
+  //-------------------------------------------------------------------
+  Cell cell;
+  if ( ! readParamCell( "cell.param" , cell ) )
+    {
+      return 1;
+    }
+  //-------------------------------------------------------------------
+
+  //-----------------
+  // Reading Cut-Off
+  //-------------------------------------------------------------------
+  CutOffMatrix cut_off;
+  if ( ! readCutOff( "cut_off.dat" , cut_off , lut_list ) )
+    {
+      return 1;
+    }
+  //-------------------------------------------------------------------
+
+  //-------------------
+  // Reading XYZ file
+  //----------------------------------------------------
+  //----------------------------------------------------
+  
+      
+  return 0;
+}
