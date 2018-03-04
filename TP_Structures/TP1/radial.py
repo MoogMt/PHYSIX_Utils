a22c6479a173941ccab0aeb66e0394f0d939d6ff
MoogMt <moog.mathieu@gmail.com>
2018-03-04 13:57:22 +0100

    deleted:    radial.002 	modified:   radial.py

--- TP_Structures/TP1/radial.py
+++ TP_Structures/TP1/radial.py
@@ -1,126 +1,98 @@
-#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-
 """
-Fonctions Radiales
+Spyder Editor
 
-#------------------------------------------------
-
-#------------------------------------------------
+This is a temporary script file.
 """
-
-#-------------------
-# Bibliotheque plot
-#---------------------------------------------
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-#---------------------------------------------
-
-# Bibliotheques de calcul
-#---------------------------------------------
+#===================================
 import numpy as np
-# Factorielle
-from scipy.special import factorial
-#---------------------------------------------
+import scipy as scp
+from scipy.misc import factorial
+import matplotlib.pyplot as plt
+#===================================
 
-#----------------------
-# Polynome de Laguerre
-#---------------------------------------------
-def laguerre(n,p,x) :
-    if n == 0 :
-        return 1.
-    elif n==1 :
-        return 1. + float(p) - float(x)
-    else :
-        lm = 1.;
-        l = 1. + float(p) - float(x)
+#==================
+# Fonctions utiles
+#======================================================================
+# Calcul la fonction radial
+# Lien: 
+def laguerre(n,p,x):
+    if n == 0:
+        return 1.0
+    elif n == 1:
+        return 1+p-x
+    else:
+        lm = 1
+        l =1+p-x
         for k in range(2,n+1):
-            lp = ( (2.*k + 1. + p - x )*l - (k+p)*lm )/( k + 1.) ;
-            lm = float( l ) ;
-            l = float( lp ) ;
-        return float(l)
-#---------------------------------------------
+            lp = ((2*k+1+p-x)*l-(k+p)*lm)/(k+1)
+            lm = l
+            l=lp
+        return l
+    return laguerre
+#---------------------------------------------------------------------
+# Calcule la fonction radiale
+# Lien:
+def radial(r,n,l):
+    # Coefficient c(l)
+    c_l= factorial(n-l-1)/(2*n*factorial(n+1)**3)*(2/(n*a0))**(l+3/2.)
+    # Retourne l'expression complète
+    return c_l*r**l*np.exp(-r/(n*a0))*laguerre(n-l-1,2*l+1,2*r/(n*a0))
+def radial_trace(r,n,l,dr):
+    # On initialise le vecteur pour la fonction radiale
+    radial_=np.ones(len(r))
+    # Facteur de normalisation
+    norm=0
+    # Boucle de r=0 à r=rmax
+    for i in range(len(r)):
+        # Calcul de la fonction radiale
+        radial_[i]=radial(i*dr,n,l);
+        # On rajoute la valeur au facteur de normalisation
+        norm+=abs(radial_[i]);
+    # On renvoit le résultat
+    return radial_/norm
+#======================================================================
 
-#--------------
-# Start Message
-#---------------------------------------
-print(" ")
-print("---------------------------------");
-print("| Calcul des fonctions radiales |")
-print("---------------------------------");
-print(" ")
-#---------------------------------------
+#=============
+# Constantes
+#============================
+# Rayon de Bohr, en Bohr
+a0=1.000000
+#=============================
 
-#--------------------------------------
-# Getting the principal quantum number
-#--------------------------------------
-n = input("Enter n: ");
-#--------------------------------------
+# Valeurs pour plot
+#===================================================================
+rmax=(float)(input("Donner la valeur max de r à plotter (Bohr): "))
+nb_r=(int)(input("Donner le nombre de points: "))
+dr=rmax/float(nb_r)
+#===================================================================
 
-#-------------------
-# Physical variables
-#-------------------------------
-a0 = 1.00# Bohr Radius
-#-------------------------------
+# Calcul de la fonction radiale
+#=====================================
+r_list=np.linspace(0,rmax,nb_r);
+#=====================================
 
-#------
-# Step
-#-------------------------------
-r_step = 300
-r_max = 10.0
-dr = r_max/float(r_step)
-#-------------------------------
-
-#-------------------------------
-print(" ")
-print("Calcul des fonctions radiales...")
-print(" ")
-#-------------------------------
-
-#-------------
-# Compute c(l)
-#----------------------------------------------------------------------------------------------
-c = np.zeros(( n ))
-for l in range( n ):
-    c[l] = float(factorial( n-l-1 ))/( 2*n* float(factorial(n+l))**3 )*( 2/(n*a0) )**(l+1.5)
-#----------------------------------------------------------------------------------------------
-
-#------------------------------------
-# Boucle sur toutes les valeurs de r
-#------------------------------------------------------------------------------------------
-R = np.zeros(( n , r_step+1 ))
-for i in range( r_step+1) :
-    # Compute r
-    r = i*dr
-    # Boucle qui couvre toutes les valeurs de l (0 < l < n)
-    for l in range( n ):
-        # Calcul de la fonction radial pour r
-        R[l,i] = c[l] * r**l * np.exp( -r/(n*a0) ) * laguerre( n-l-1 , 2*l+1 , 2*r/(n*a0) ) ;
-#------------------------------------------------------------------------------------------
-
-#----------------
-# Normalisation
-#------------------------------
-norm = 0.0000000000
-for l in range( n ):
-    for i in range( r_step+1 ):
-        norm += (i*dr*R[l,i])**2*dr
-    R[l,:] /= np.sqrt(norm)
-#------------------------------
-
-#--------------------------------------------------
-# Plot toutes les fonctions radiales pour n fixe
-#--------------------------------------------------
-r = np.linspace(0,r_max,r_step+1)
-for l in range(n):
-      plt.plot( r , R[l,:] , label="check")
-      plt.xlabel('r (Bohr)')
-      plt.ylabel('R_n(r)')
+# Plots pour plusieurs n
+#==============================================
+plt.figure(1)
+plt.plot(r_list,radial_trace(r_list,1,0,dr))
+plt.plot(r_list,radial_trace(r_list,2,0,dr))
+plt.plot(r_list,radial_trace(r_list,3,0,dr))
+plt.plot(r_list,radial_trace(r_list,4,0,dr))
+plt.xlabel("r (Bohr)")
+plt.ylabel("R_n,l(r)")
+plt.legend(["n=1,l=0","n=2, l=0","n=3, l=0","n=4,l=0"])
 plt.show()
-#---------------------------------------------------
-      
-#-------------------------------
-print(" ")
-print("Travail termine!")
-print(" ")
-#-------------------------------
+#=============================================
+
+# Plots pour n fix et tout les l
+#=============================================
+plt.figure(2)
+plt.plot(r_list,radial_trace(r_list,3,0,dr))
+plt.plot(r_list,radial_trace(r_list,3,1,dr))
+plt.plot(r_list,radial_trace(r_list,3,2,dr))
+plt.xlabel("r (Bohr)")
+plt.ylabel("R_n,l(r)")
+plt.legend(["n=3,l=0","n=3, l=1","n=3, l=2"])
+plt.show()
+#=============================================
