9c531d7464d4a2383fffb64372a7c9aa9fb51f90
CondensedOtters <moog.mathieu@gmail.com>
2018-03-06 23:10:02 +0100

    modified:   GPlib/Cpp/elf.cpp 	ELF/load_elf_vmd

--- GPlib/Cpp/elf.cpp
+++ GPlib/Cpp/elf.cpp
@@ -40,15 +40,13 @@ int main( void )
   //--------
   // Output
   //-------------------------------------
-  std::ofstream output("output.dat");
+  std::ofstream output("ELF_out.dat");
   //-------------------------------------
   
   //---------------
   // Initializers
   //-------------------------------------
   AtomList  atom_list; // Atoms in cell
-  AllTypeLUT lut_list; // LUT for types
-  ContactMatrix    cm; // Contact matrix
   //-------------------------------------
 
   //--------------------
@@ -61,21 +59,76 @@ int main( void )
     }
   //-------------------------------------------------------------------
 
-  //-----------------
-  // Reading Cut-Off
+  std::string line;
+  int nb_atoms=0;
+  double ox, oy, oz;
+  int nb_vox[3];
+  double x,y,z;
+  int atom_type;
+  double dummy;
+  double cell_x[3], cell_y[3], cell_z[3];
+  AtomList atoms;
+  
+  //-------------------------------
+  // Read useless two first lines
   //-------------------------------------------------------------------
-  CutOffMatrix cut_off;
-  if ( ! readCutOff( "cut_off.dat" , cut_off , lut_list ) )
+  for ( int i=0; i<2; i++)
     {
-      return 1;
+  
+      if ( ! std::getline(input,line) )
+	{
+	  std::cout << "Problem with the file ELF.cube" <<std::endl;
+	  return 1;
+	}
     }
   //-------------------------------------------------------------------
 
-  //-------------------
-  // Reading XYZ file
-  //----------------------------------------------------
-  //----------------------------------------------------
+  //----------------------------------------------------------------------
+  // Reading number of atoms and origin of the origin point of the density
+  //------------------------------------------------------------------------
+  if( std::getline(input,line) )
+    {
+      std::istringstream it_string(line);
+      if ( ! ( it_string >> nb_atoms >> ox >> oy >> oz ) ) return 1;
+    }
+  else return 1;
+  //-----------------------------------------------------------------------
+
+  //--------------------------------------------
+  // Reading number of voxels and cell vectors 
+  //-----------------------------------------------------------------------
+  for( int i=0 ; i<3 ; i++ )
+    {
+      if( std::getline(input,line) )
+	{
+	  if ( ! ( it_string >> nb_vox[i] >> cell_x[i] >> cell_y[i] >> cell_z[i] ) ) return 1;
+	}
+      else return 1;
+    }
+  //-----------------------------------------------------------------------
+
+  //---------------------------
+  // Reading atomic positions
+  //----------------------------------------------------------------------------------------
+  // Initiate the atom_list
+  //-------------------------------------
+  atoms.assign( nb_atoms, "");
+  atoms.x.assign( nb_atoms, 0);
+  atoms.y.assign( nb_atoms, 0);
+  atoms.z.assign( nb_atoms, 0);
+  atoms.index.assign( nb_atoms, 0);
+  //-------------------------------------
+  // Reading the atomic positions and all
+  //----------------------------------------------------------------------------------
+  for( int i=0 ; i < nb_atoms ; i++ )
+    {
+      if ( std::getline(input,line) )
+	{
+	  if ( ! ( it_string >> atom_type >> dummy >> x >> y >> z ) ) return 1;
+	}
+      else return 1;
+    }
+  //----------------------------------------------------------------------------------------
   
-      
   return 0;
 }
