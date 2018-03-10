773858c1cabe7bab59f582304227dd353c36ecd1
MoogMt <moog.mathieu@gmail.com>
2018-03-10 18:06:14 +0100

    modified:   GPlib/Julia/cell.jl 	modified:   GPlib/Julia/cubefile.jl 	modified:   GPlib/Julia/utils.jl

--- GPlib/Julia/cell.jl
+++ GPlib/Julia/cell.jl
@@ -12,6 +12,11 @@ mutable struct Cell_vec
     v1::Vector{Real}
     v2::Vector{Real}
     v3::Vector{Real}
+end
+mutable struct Cell_matrix
+    matrix::Array{Real}
+end
 
 print("cell_mod loaded");
+
 end
--- GPlib/Julia/cubefile.jl
+++ GPlib/Julia/cubefile.jl
@@ -1,13 +1,38 @@
 module cube_mod
 
-include("atom.jl");
+include("atoms.jl");
 include("cell.jl")
 
 # Reads a cube file and returns all or parts of its informations
-function readCube{}()
+function readCube{T1<:AbstractString}( file_name::T1)
+    # Open file
+    file=open(file_name);
+    lines=readlines(file);
+    close(file);
+
+    nb_atoms = parse(Int,split(lines[3])[1]);
+
+    center=Vector{Real}(3)
+    for i=1,3
+        center[i] = parse(Real, split( lines[3] )[i+1] );
+    end
+
+    nb_vox=Vector{Real}(3)
+    for i=1,3
+        nb_vox[i] = parse(Real, split( lines[3+i] )[1] )
+    end
+
+    cell_matrix=Array{Real}(3,3)
+    for i=1,3
+        for j=1,3
+            cell_matrix[i,j] = parse(Real, split( lines[4+i] )[1+j] )*0.52917721067
+        end
+    end
+    cell_mod.Cell_matrix=cell_matrix
 
     
-    return nb_atoms, atom_list, density
+
+    return nb_atoms, nb_vox, cell_matrix
 end
 
 end
