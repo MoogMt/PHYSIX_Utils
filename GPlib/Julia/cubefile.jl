5f63216731c85566037ecee21d88d88cdf4dec3b
CondensedOtters <moog.mathieu@gmail.com>
2018-03-16 12:41:41 +0100

    modified:   GPlib/Julia/cubefile.jl

--- GPlib/Julia/cubefile.jl
+++ GPlib/Julia/cubefile.jl
@@ -5,15 +5,13 @@ include("cell.jl");
 
 #-----------------------------------------------------------------------
 mutable struct Volume
-    position::Array{Real}
-    value::Vector{Real}
+    matrix::Array{Real}
     function Volume()
-        new(Array{Real}(0,3),Vector{Real}())
+        new( Array{Real}(0,4) )
     end
     function Volume{T2 <: Real}( nb_vox::Vector{T2} )
         if size(nb_vox)[1] == 3
-            nb_tot=nb_vox[1]*nb_vox[2]*nb_vox[3]
-            new(Array{Real}(nb_tot,3),Vector{Real}(nb_tot))
+            new( Array{Real}(nb_vox[1]*nb_vox[2]*nb_vox[3],4) )
         else
             print("/!\\ Error: Wrong size for the number of voxels.");
         end
@@ -42,8 +40,7 @@ function readCube{T1<:AbstractString}( file_name::T1)
     #-----------------------------------------------------
     center=Vector{Real}(3)
     for i=1:3
-        center[i] = parse(Float64, split( lines[3] )[i+1] );
-    end
+dfvhsdefa    end
     #-----------------------------------------------------
 
     #-------------------------------------
@@ -85,14 +82,37 @@ function readCube{T1<:AbstractString}( file_name::T1)
     nb_col=6
     volume = Volume(nb_tot)
     offset=6+nb_atoms
-    for i=1:nb_tot/nb_col
+    x=0; y=0; z=0;
+    for i=0:nb_tot/nb_col-1
         for j=1:nb_col
-            volume.value[i*nb_col+j]=parse(Float64, split( lines[offset+i])[j] )
+            volume.matrix[1,i*nb_col+j] = x
+            volume.matrix[2,i*nb_col+j] = y
+            volume.matrix[3,i*nb_col+j] = z
+            volume.matrix[4,i*nb_col+j] = parse(Float64, split( lines[offset+i])[j] )
+            z++
+            if z == nb_atoms
+                z=0;
+                y=y+1;
+            end
+            if y == nb_atoms
+                y=0;
+                x=x+1;
+            end
         end
+    end
+    # Scaling positions
+    dV=[0,0,0]
+    for i=1:3
+        for j=1:3
+            dV[i] += cell_matrix[i,j];
+        end
+        volume.matrix[i,:] = volume.matrix[i,:]*dV[i];
     end
     #----------------------------------------------------
 
     return atom_list, cell_matrix, volume
 end
+
+print("Cube Module Loaded!\n")
 
 end
