7a60cbe0db3622b9c0102f666f3e4615f2682d28
MoogMt <moog.mathieu@gmail.com>
2018-03-18 23:44:01 +0100

    modified:   GPlib/Julia/pressure.jl

--- GPlib/Julia/pressure.jl
+++ GPlib/Julia/pressure.jl
@@ -1,6 +1,6 @@
 module pressure
 
-function readPressure( file_name::AbstractString , diag::Bool )
+function readPressure( file_name::AbstractString , diag::Bool , stride::Int)
     #--------------
     # Reading file
     #----------------------
@@ -10,22 +10,28 @@ function readPressure( file_name::AbstractString , diag::Bool )
     #-----------------------
 
     nb_lines=size(lines)[1]
-    pressure=zeros(nb_lines/4,1)
+    nb_pressure_points=nb_lines/(4*stride)
+    pressure=zeros(nb_pressure_points,1)
     if ! diag
-        for i=1:nb_lines/4.
+        for i=1:nb_pressure_points
             for j=2:4
-                pressure[î]= parse(Float64,split(lines[i+j])[j-1])
+                pressure[î]= parse(Float64,split(lines[i*stride+j])[j-1])
             end
             pressure[i] = pressure[i]/3.
         end
     else
         pressure_matrix=zeros(3,3)
-        for i=1:nb_lines/4
+        for i=1:nb_pressure_points
             for j=2:4
                 for k=1:3
-                    pressure_matrix[j,k]=
+                    pressure_matrix[j,k]=split(lines[i*stride+j])[k]
                 end
             end
+            eigen_press=eigvals(pressure_matrix)
+            for l=1:3
+                pressure[i]=eigen_press[l]
+            end
+            pressure[i] = pressure[i]/3.
         end
     end
 
