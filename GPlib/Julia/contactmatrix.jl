86d3558052319abeb5c505dde284d3d0fc081d25
MoogMt <moog.mathieu@gmail.com>
2018-03-20 00:14:24 +0100

    modified:   GPlib/Julia/contactmatrix.jl

--- GPlib/Julia/contactmatrix.jl
+++ GPlib/Julia/contactmatrix.jl
@@ -1,7 +1,28 @@
 module contact_matrix
 
+import atom_mod.AtomList
+importall atom_mod
+
 mutable struct ContactMatrix
-    ContactMatrix::Array{Real,2}
+    ContactMatrix::Array{Real}
+    function ContactMatrix()
+        new(Array{Real}(0,0))
+    end
+    function ContactMatrix{ T1 <: AtomList }( atoms::T1 )
+        nb_atoms=size(atoms)[1]
+        matrix=zeros(nb_atoms,nb_atoms)
+        for i=1:nb_atoms-1
+            for j=i+1:nb_atoms
+                distance=0
+                for k=1:3
+                    distance += (atoms.positions[i,k]-atoms.positions[j,k])^2
+                end
+                distance=sqrt(distance)
+                matrix[i,j]=distance
+                matrix[j,i]=distance
+            end
+        end
+    end
 end
 
 mutable struct ContactVector
