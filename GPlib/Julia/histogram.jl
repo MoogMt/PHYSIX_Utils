aba7a21ad931e4b86022fce13910d6a85fa22eec
CondensedOtters <moog.mathieu@gmail.com>
2018-03-22 18:54:47 +0100

    modified:   histogram.jl

--- GPlib/Julia/histogram.jl
+++ GPlib/Julia/histogram.jl
@@ -1,18 +1,26 @@
 module histogram
 
 #-----------------------------------------------------------------------
-mutable struct bin
+mutable struct Bin
     begin_box::Real
     end_box::Real
     value::Real
 end
 
-mutable struct histogram
+mutable struct Histogram
     begins_box::Vector{Real}
     ends_box::Vector{Real}
     values::Vector{Real}
-    function Volume()
-        new( Array{Real}(0,4) )
+    function Histogram()
+        new( [],[],[] )
+    end
+    function Histogram{ T1 <: Real, T2 <: Int }( data::Array{T1}, nb_box::T2 )
+        if size(data)[2] == 1
+            
+        else
+            print("Error on size of data array. \n")
+            return
+        end
     end
 end
 #-----------------------------------------------------------------------
