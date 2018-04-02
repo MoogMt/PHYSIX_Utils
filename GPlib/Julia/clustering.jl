af469b1abfc9746a6773cec71af1650e4ed9d63b
MoogMt <moog.mathieu@gmail.com>
2018-04-02 18:29:55 +0200

    new file:   clustering.jl

---
+++ GPlib/Julia/clustering.jl
@@ -0,0 +1,11 @@
+module clustering
+
+    function kmedoids{ T1 <: Real, T2 <: Int }( data::Vector{T1}, nb_clusters::T2 )
+
+    end
+
+    function daura{ T1 <: Real }( data::Vector{T1} )
+        
+    end
+
+end
