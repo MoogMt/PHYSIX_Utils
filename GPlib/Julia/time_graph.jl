2d93245f810007753414ee6036e1a4eac753309c
CondensedOtters <moog.mathieu@gmail.com>
2018-06-11 20:09:13 +0200

    modified:   GPlib/Julia/time_graph.jl

--- GPlib/Julia/time_graph.jl
+++ GPlib/Julia/time_graph.jl
@@ -18,13 +18,12 @@ nb_atoms=size(atoms[1].names)[1]
 
 cutoff=1.7
 atom=1
-bond_matrix=zeros(nb_steps,nb_atoms)
-for i=1:nb_steps
-    for j=33:nb_atoms
-        if j != atom
-            if cell_mod.distance(atoms[i],cell,atom,j) < 1.7
-                print("progress: ",i/nb_steps*100,"\n")
-                bond_matrix[i,j] += 1
+bond_matrix=zeros(nb_steps,Int(trunc((nb_atoms*(nb_atoms-1)/2))))
+for step=1:nb_steps
+    for i=1:nb_atoms
+        for j=i+1:nb_atoms
+            if cell_mod.distance(atoms[step],cell,i,j) < 1.7
+                bond_matrix[step,i+j] += 1
             end
         end
     end
@@ -37,20 +36,22 @@ function autocorrelation(x,N,i,M)
             C[n] += x[k]*x[k-n]
         end
     end
-    return C/M
+    return C/C[1]
 end
 
-steps=15000
+steps=5000
 nb_steps=30000
 time_corr_tot=zeros(steps)
-for i=33:nb_atoms
-    print("progress:",(i-32)/64*100,"%\n")
-    time_corr_loc=autocorrelation(bond_matrix[:,i-32],steps,steps,nb_steps-steps)
-    for j=1:M
+for i=1:Int(trunc((nb_atoms*(nb_atoms-1)/2)))
+    print("progress:",i/*(nb_atoms*(nb_atoms-1)/2)*100,"%\n")
+    time_corr_loc=autocorrelation(bond_matrix[:,i],steps,steps,nb_steps-steps)
+    for j=1:steps
         time_corr_tot[j] += time_corr_loc[j]
     end
 end
-time_corr_tot /= 64
+for i=1:steps
+    time_corr_tot[i] = time_corr_tot[i]/(nb_atoms*(nb_atoms-1)/2)
+end
 
 file_check=open(string("/home/moogmt/time_corr_",current_volume,"-",T,".dat"),"w")
 for i=1:steps
