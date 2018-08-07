93193fcbcb2a07cc7826f81d38c00cdce69a7d93
MoogMt <moog.mathieu@gmail.com>
2018-08-07 18:28:28 +0200

    modified:   GPlib/Julia/cluster_convergence.jl

--- GPlib/Julia/cluster_convergence.jl
+++ GPlib/Julia/cluster_convergence.jl
@@ -1,6 +1,3 @@
-    # Loading file
-include("contactmatrix.jl")
-
 # TESTED AND FUNCTIONNALS
 function compute_distance( data )
     size_data=size(data)[1]
@@ -78,7 +75,7 @@ function voronoiAssignAll{ T1 <: Int, T2 <: Real, T3 <: Int, T4 <: Int}( nb_stru
     cluster_indexs = zeros(Int, nb_structures )
     assignments = zeros(Int, nb_clusters, nb_structures )
     # Contains sizer of clusters
-    cluster_sizes = zeros(Int, nb_clusters)
+    cluster_sizes=zeros(Int,nb_clusters)
     for structure=1:nb_structures
         cluster_indexs[ structure ] = voronoiAssignSingle( distance_matrix, nb_clusters, cluster_centers, structure )
         cluster_sizes[ cluster_indexs[structure] ] += 1
@@ -102,11 +99,11 @@ function computeCost{ T1 <: Int, T2 <: Real, T3 <: Int, T4 <: Int }( n_structure
     end
     return cost
 end
-function updateCenters{ T1 <: Real, T2 <: Int, T3 <: Int, T4 <: Int , T5 <: Int }( distance_matrix::Array{T1,2} , n_clusters::T2, cluster_centers::Vector{T3}, cluster_sizes::Vector{T4}, assignments::Array{2,T5} )
+function updateCenters{ T1 <: Real, T2 <: Int, T3 <: Int, T4 <: Int , T5 <: Int }( distance_matrix::Array{T1,2} , n_clusters::T2, cluster_centers::Vector{T3}, cluster_sizes::Vector{T4}, assignments::Array{T5,2} )
     for cluster=1:n_clusters
         new_center = cluster_centers[ cluster ]
         cost_min = sum(distance_matrix[ assignments[ cluster,1], assignments[ cluster ,1:cluster_sizes[ cluster ] ] ])
-        for structure_in_cluster=2:cluster_sizes[ cluster]
+        for structure_in_cluster=2:cluster_sizes[ cluster ]
             cost = sum(distance_matrix[ assignments[ cluster, structure_in_cluster ], assignments[ cluster ,1:cluster_sizes[ cluster ] ] ])
             if cost < cost_min
                 cost_min=cost
@@ -118,72 +115,62 @@ function updateCenters{ T1 <: Real, T2 <: Int, T3 <: Int, T4 <: Int , T5 <: Int 
 end
 function kmedoidClustering{ T1 <: Int, T2 <: Real, T3 <: Int, T4 <: Real }( n_structures::T1 , distance_matrix::Array{T2,2}, n_clusters::T3 , precision::T4 )
     # Initialization of centers
-    cluster_centers=initializeCenters(nb_structures, distance_matrix, nb_clusters )
+    cluster_centers=initializeCenters(n_structures, distance_matrix, n_clusters )
     # Assign all clusters
-    cluster_indexs, cluster_sizes, assignments =voronoiAssignAll( nb_structures, distance_matrix, nb_clusters, cluster_centers )
+    cluster_indexs, cluster_sizes, assignments =voronoiAssignAll( n_structures, distance_matrix, n_clusters, cluster_centers )
     # Compute original cost
-    old_cost=computeCost(nb_structures,distance_matrix,cluster_centers,cluster_indexs)
+    old_cost=computeCost(n_structures,distance_matrix,cluster_centers,cluster_indexs)
 
     old_cost=1
     while true
-        voronoiAssignAll( nb_structures, distance_matrix, nb_clusters, cluster_centers,cluster_indexs, cluster_sizes, assignments  )
-        cost=computeCost(nb_structures,distance_matrix,cluster_centers,cluster_indexs)
+        voronoiAssignAll( n_structures, distance_matrix, n_clusters, cluster_centers,cluster_indexs, cluster_sizes, assignments  )
+        cost=computeCost( n_structures, distance_matrix, cluster_centers, cluster_indexs)
         if abs(cost-old_cost) < precision
             break
         end
         old_cost=cost
         updateCenters( distance_matrix, n_clusters, cluster_centers, cluster_sizes, assignments )
     end
-    return cluster_indexs, cluster_centers, assignments
+    return cluster_indexs, cluster_centers, cluster_sizes, assignments
 end
 
 # Definition of the points
-points=zeros(150,2)
+points=zeros(200,2)
 for i=1:50
     points[i,:]=rand(2)
 end
 for i=51:100
     points[i,:]=rand(2)
 end
-points[51:100,1] += 2
+points[51:100,1] += 0.8
 for i=101:150
     points[i,:]=rand(2)
 end
-points[101:150,2] += 2
+points[101:150,2] += 0.8
+for i=151:200
+    points[i,:]=rand(2)
+end
+points[151:200,1] += 0.8
+points[151:200,2] += 0.8
 # Compute the distance between all points
 distance_matrix=compute_distance(points)
 
 # Cluster parameters
-n_clusters=3
+n_clusters=5
 precision=0.00000000000001
 n_structures=size(points)[1]
 
-cluster_index, cluster_centers, assignments = kmedoidClustering( n_structures, distance_matrix, n_clusters )
-old_cost=computeCost( n_structures, distance_matrix, cluster_centers, cluster_indexs)
-
-while true
-    cluster_index, cluster_centers, assignments = kmedoidClustering( n_structures, distance_matrix, n_clusters )
-    cost=computeCost( n_structures, distance_matrix, cluster_centers, cluster_indexs)
-    if abs(cost-old_cost) < precision
-        break
-    end
-end
+cluster_indexs, cluster_centers, cluster_sizes, assignments = kmedoidClustering( n_structures, distance_matrix, n_clusters, precision )
+old_cost=computeCost( n_structures, distance_matrix, cluster_centers, cluster_indexs )
 
 using PyPlot
-
-figure()
-plot(points[:,1],points[:,2],"r.")
-for i=1:n_clusters
-    xlabel("x")
-    ylabel("y")
-    plot( points[cluster_centers[i],1] , points[cluster_centers[i],2] , ".", color=[ rand(),rand(),rand() ] )
-end
 
 figure()
 
 plot( points[cluster_centers[ 1 ] ,1 ] , points[cluster_centers[ 1 ],2] , "rd"  )
 plot( points[cluster_centers[ 2 ] ,1 ] , points[cluster_centers[ 2 ],2] , "bd"  )
 plot( points[cluster_centers[ 3 ] ,1 ] , points[cluster_centers[ 3 ],2] , "gd"  )
+plot( points[cluster_centers[ 4 ] ,1 ] , points[cluster_centers[ 4 ],2] , "kd"  )
 
 for j=1:cluster_sizes[ 1 ]
     plot( points[ assignments[ 1, j ] , 1 ] , points[ assignments[ 1, j ] ,2 ] , "r."  )
@@ -193,4 +180,7 @@ for j=1:cluster_sizes[ 2 ]
 end
 for j=1:cluster_sizes[ 3 ]
     plot( points[ assignments[ 3, j ] , 1 ] , points[ assignments[ 3, j ] ,2 ] , "g."  )
+end
+for j=1:cluster_sizes[ 4 ]
+    plot( points[ assignments[ 4, j ] , 1 ] , points[ assignments[ 4, j ] ,2 ] , "k."  )
 end
