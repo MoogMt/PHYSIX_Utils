2e4129a246f1aaf0d9d1de5ca34e693de959c5e7
MoogMt <moog.mathieu@gmail.com>
2018-08-07 03:14:57 +0200

    modified:   GPlib/Julia/cluster_convergence.jl

--- GPlib/Julia/cluster_convergence.jl
+++ GPlib/Julia/cluster_convergence.jl
@@ -7,18 +7,18 @@ function compute_distance( data )
     matrix=zeros(size_data,size_data)
     for i=1:size_data
         for j=1:size_data
-            matrix[i,j] = sqrt(sum( (points[i,:]-points[j,:]).*(points[i,:]-points[j,:]) ))
+            matrix[i,j] = sqrt(sum( (data[i,:]-data[j,:]).*(data[i,:]-data[j,:]) ))
         end
     end
     return matrix
 end
-function swap(table, index1, index2 )
+function swap{ T1 <: Real, T2 <: Int, T3 <: Int }( table::Vector{T1}, index1::T2, index2::T3 )
     stock=table[index1]
     table[index1]=table[index2]
     table[index2]=stock
     return
 end
-function initialize_medoid( distance_matrix , n_structures , n_clusters  )
+function initializeCenters{ T1 <: Int, T2 <: Real , T3 <: Int}( n_structures::T1 , distance_matrix::Array{T2,2}  , n_clusters::T3  )
     # Bookkeep
     available=ones(Int,n_structures)
 
@@ -86,11 +86,12 @@ function voronoiAssignAll{ T1 <: Int, T2 <: Real, T3 <: Int, T4 <: Int}( nb_stru
     end
     return cluster_indexs, cluster_sizes, assignments
 end
-function voronoiAssignAll{ T1 <: Int, T2 <: Real, T3 <: Int, T4 <: Int, T5 <: Int, T6 <: Int, T7 <: Int }( nb_structures::T1 , distance_matrix::Array{T2,2}, nb_clusters::T3, cluster_centers::Vector{T4}, cluster_indexs::Vector{T5}, cluster_sizes::Vector{T6}, assignments::Array{T7,2} )
-    for structure=1:nb_structures
+function voronoiAssignAll{ T1 <: Int, T2 <: Real, T3 <: Int, T4 <: Int, T5 <: Int, T6 <: Int, T7 <: Int }( n_structures::T1 , distance_matrix::Array{T2,2}, nb_clusters::T3, cluster_centers::Vector{T4}, cluster_indexs::Vector{T5}, cluster_sizes::Vector{T6}, assignments::Array{T7,2} )
+    cluster_sizes=zeros(Int,nb_clusters)
+    for structure=1:n_structures
         cluster_indexs[ structure ] = voronoiAssignSingle( distance_matrix, nb_clusters, cluster_centers, structure )
         cluster_sizes[ cluster_indexs[structure] ] += 1
-        assignemnts[ cluster_indexs[structure], cluster_sizes[ cluster_indexs[structure] ] ] = cluster_indexs[ structure ]
+        assignments[ cluster_indexs[ structure ], cluster_sizes[ cluster_indexs[ structure ] ] ] = cluster_indexs[ structure ]
     end
     return
 end
@@ -101,13 +102,32 @@ function computeCost{ T1 <: Int, T2 <: Real, T3 <: Int, T4 <: Int }( n_structure
     end
     return cost
 end
+
+
+# TESTED
+function updateCenters( distance_matrix, n_clusters, cluster_centers, cluster_sizes, assignments )
+    for cluster=1:n_clusters
+        new_center = cluster_centers[ cluster ]
+        cost_min = sum(distance_matrix[ assignments[ cluster,1], assignments[ cluster ,1:cluster_sizes[ cluster ] ] ])
+        for structure_in_cluster=2:cluster_sizes[ cluster]
+            cost = sum(distance_matrix[ assignments[ cluster, structure_in_cluster ], assignments[ cluster ,1:cluster_sizes[ cluster ] ] ])
+            if cost < cost_min
+                cost_min=cost
+                new_center = assignments[ cluster , structure_in_cluster ]
+            end
+        end
+        cluster_centers[ cluster ] = new_center
+    end
+end
+
+
 # Definition of the points
-points=zeros(100,3)
+points=zeros(100,2)
 for i=1:50
-    points[i,:]=rand(3)
+    points[i,:]=rand(2)
 end
 for i=51:100
-    points[i,:]=rand(3)+10
+    points[i,:]=rand(2)+10
 end
 
 # Compute the distance between all points
@@ -118,100 +138,24 @@ nb_clusters=2
 nb_structures=size(points)[1]
 
 # Initialization of centers
-cluster_centers=initialize_medoid(distance_matrix, nb_structures, nb_clusters )
+cluster_centers=initializeCenters(nb_structures, distance_matrix, nb_clusters )
 # Assign all clusters
 cluster_indexs, cluster_sizes, assignments =voronoiAssignAll( nb_structures, distance_matrix, nb_clusters, cluster_centers )
 # Compute original cost
-cost=computeCost(nb_structures,distance_matrix,cluster_centers,cluster_indexs)
+old_cost=computeCost(nb_structures,distance_matrix,cluster_centers,cluster_indexs)
 
-
-function kmenoid_clustering{ T1 <: Int, T2 <: Int, T3 <: Real }( n_clusters::T1 , n_structures::T2, distance_matrix::Vector{Real} )
-
-    cluster_centers=zeros(n_clusters)
-
-    initialize_medoid( distance_matrix, n_structures, n_ clusters, cluster_centers
-
-    old_cost=1
-
-    # Minimization of medoids
-    #  - Look up table
-    cluster2i=zeros(n_clusters,n_structures)
-    i2cluster=zeros(n_structures)
-    # - Size of clusters
-    cluster_sizes=zeros(n_clusters)
-    while true
-        # Voronoi assignment
-        cluster_sizes=zeros(n_clusters)
-        for i=1:n_structures
-            dmin=1.0
-            for j=1:n_structures
-                if distance_matrix[i,j] < dmin
-                    dmin = distance_matrix[i,j]
-                    i2cluster[i]=j
-                end
-            end
-            j=i2cluster[i]
-            cluster_size[j] += 1
-            cluster2i[j,cluster_size[j]]=i
-        end
-
-        # Cost of the clustering
-        cost=0
-        for i=1:n_structures
-            cost += distance_matrix[i,cluster2i[i]]
-        end
-        # Exit if nothing changed (convergence reached)
-        if cost == old_cost
-            break
-        end
-
-        # Updating Medoid
-        for i=1:n_clusters
-            dmin=1.
-            for j=1:cluster_size[i]
-                dtmp = sum(distance_matrix[ cluster2i[i,j], cluster2i[ i, 1:cluster_size[i] ] ])
-                if dtmp < dmin
-                    dmin=dtmp
-                    newcenter=cluster2i[i,j]
-                end
-            end
-            cluster_center[i] = newcenter
-        end
+old_cost=1
+while true
+    voronoiAssignAll( nb_structures, distance_matrix, nb_clusters, cluster_centers,cluster_indexs, cluster_sizes, assignments  )
+    cost=computeCost(nb_structures,distance_matrix,cluster_centers,cluster_indexs)
+    if abs(cost-old_cost) < 0.0000001
+        break
     end
-
-    # Sorting cluster
-    cluster_member=cluster2i
-    cluster_ranking=zeros(n_clusters)
-    for i=1:n_clusters
-        cluster_ranking[i]=i
-    end
-    for i=1:n_clusters-1
-        for j=i+1:n_clusters
-            if cluster_sizes[i] > cluster_sizes[j]
-                swap(cluster_size,i,j)
-                swap(cluster_ranking,i,j)
-            end
-        end
-    end
-
-    assigned=zeros(n_structures)
-    for i=1:n_clusters
-        for j=1:cluster_size[i]
-            assigned[cluster_member[i,j]] = assigned[cluster_member[i,j]]+1
-        end
-    end
-
-    for i=1:n_structure
-        if assigned[i] != 1
-            print("Oups")
-        end
-    end
-
-    file=open(string(pwd(),"/log-clusters.dat"),"w")
-    for i=1:n_clusters
-        write(file,string(i," ",cluster[ i,cluster_sizes(i) ] ) )
-    end
-    close(file)
-
-    return cluster_centers
+    old_cost=cost
+    updateCenters( distance_matrix, n_clusters, cluster_centers, cluster_sizes, assignments )
 end
+
+using PyPlot
+
+plot(points[1:50,:],"r.")
+plot(points[1:50,:],"b.")
