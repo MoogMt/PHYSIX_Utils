f73b148218d847112696a67f794b4d1be8b33788
MoogMt <moog.mathieu@gmail.com>
2018-08-13 03:51:42 +0200

    modified:   GPlib/Julia/cluster_convergence.jl

--- GPlib/Julia/cluster_convergence.jl
+++ GPlib/Julia/cluster_convergence.jl
@@ -281,35 +281,36 @@ function dauraClustering{ T1 <: Int, T2 <: Real, T3 <: Real }( n_elements::T1, d
     cluster_sizes = cluster_sizes[1:n_cluster]
     used=[]
     # Voronoi assignements
-    assignements=zeros(n_elements)
+    index_data = zeros(n_elements)
     for element=1:n_elements
         min_dist=0
         if cluster_centers[1] != element
             min_dist = distance_matrix[ cluster_center[1], element ]
-            assignement[ element ] = cluster_center[1]
+            index_data[ element ] = cluster_center[1]
             for i=2:size(cluster_centers)[1]
                 if cluster_center[i] != element
                     dist = distance_matrix[ cluster_center[i], element ]
                     if dist < min dist
-                        assignement[ element ] = i
+                        index_data[ element ] = i
                     end
                 end
             end
+
         else
             min_dist = distance_matrix[ cluster_center[2], element  ]
-            assignement[ element ] = cluster_centers[2]
+            index_data[ element ] = cluster_centers[2]
             for i=3:size(cluster_centers)[1]
                 if cluster_center[i] != element
                     dist = distance_matrix[ cluster_center[i], element ]
                     if dist < min dist
-                        assignement[ element ] = i
+                        index_data[ element ] = i
                     end
                 end
             end
         end
     end
     # Voronoi assignement of the points
-    return cluster_centers, cluster_sizes, assignements
+    return cluster_centers, cluster_sizes, index_data
 end
 #==============================================================================#
 
@@ -379,6 +380,8 @@ for j=1:cluster_sizes[ 4 ]
     plot( points[ assignments[ 4, j ] , 1 ] , points[ assignments[ 4, j ] ,2 ] , "k."  )
 end
 
+#==========#
+# K-medoid
 #==============================================================================#
 
 include("contactmatrix.jl")
@@ -514,5 +517,122 @@ for cluster=1:n_clusters
     close(file)
 end
 
-# Test of Daura
-dauraClustering( nb_steps , distance_matrix , 0.3 )
+#========#
+# Daura
+#==============================================================================#
+
+include("contactmatrix.jl")
+
+V=8.82
+T=3000
+
+ps2fs=0.001
+timestep=0.5
+stride = 1
+unit=ps2fs*timestep*stride
+start_time=5
+start_step=Int(start_time/unit)
+nbC=32
+nbO=2*nbC
+
+folder=string("/home/moogmt/CO2/CO2_AIMD/",V,"/",T,"K/")
+file="TRAJEC_wrapped.xyz"
+
+traj = filexyz.read( string(folder,file), stride, start_step )
+cell=cell_mod.Cell_param( V, V, V )
+
+nb_steps=size(traj)[1]
+nb_atoms=size(traj[1].names)[1]
+
+# Training set
+nb_dim=10
+data_set=zeros(nb_steps*nbC,nb_dim)
+fileC=open(string(folder,"distancesNN.dat"),"w")
+for step=1:nb_steps
+    print("Progress:", step/nb_steps*100,"%\n")
+	for carbon=1:nbC
+		distances = zeros(nbO)
+		for oxygen=1:nbO
+			distances[oxygen] = cell_mod.distance(traj[step],cell,carbon,oxygen+nbC)
+		end
+		index=sortmatrix( distances )
+        maxN=4
+		for i=1:maxN
+			data_set[ carbon+nbC*(step-1), i ] = distances[ i ]
+            write(fileC, string(distances[i]," ") )
+		end
+        a=cell_mod.distance(traj[step],cell,carbon,Int(index[1]+nbC))
+        b=cell_mod.distance(traj[step],cell,carbon,Int(index[2]+nbC))
+        c=cell_mod.distance(traj[step],cell,Int(index[1]+nbC),Int(index[2]+nbC))
+        angle=acosd((a*a+b*b-c*c)/(2*a*b))
+        count=maxN+1
+        for i=1:maxN-1
+            for j=i+1:maxN
+                a=cell_mod.distance(traj[step],cell,carbon,Int(index[i]+nbC))
+                b=cell_mod.distance(traj[step],cell,carbon,Int(index[j]+nbC))
+                c=cell_mod.distance(traj[step],cell,Int(index[i]+nbC),Int(index[j]+nbC))
+                angle=acosd((a*a+b*b-c*c)/(2*a*b))
+                write(fileC,string(angle," "))
+                data_set[ carbon+nbC*(step-1), count ] = angle
+                count += 1
+            end
+        end
+        write(fileC,string("\n"))
+	end
+end
+close(fileC)
+
+n_train=4000
+n_dim_analysis=5
+data_train=data_set[1:n_train,1:n_dim_analysis ]
+data_predict=data_set[n_train+1:nb_steps,1:n_dim_analysis ]
+data_set=[]
+
+max=data_train[1,:]
+min=data_train[1,:]
+for i=1:n_train
+    for j=1:n_dim_analysis
+        if max[j] < data_train[i,j]
+            max[j] = data_train[i,j]
+        end
+        if min[j] > data_train[i,j]
+            min[j] = data_train[i,j]
+        end
+    end
+end
+
+print("Computing distance matrix\n")
+distance_matrix=computeDistanceMatrix( data_train , n_dim_analysis, max, min )
+
+cut_off = 0.3
+
+print("Clustering\n")
+cluster_centers, cluster_sizes, index_cluster = dauraClustering( nb_steps , distance_matrix , cut_off )
+
+n_cluster=size(cluster_centers)[1]
+
+print("Printing cluster centers\n")
+file = open( string( folder, "center_cluster-",n_dim_analysis,".dat" ), "w" )
+for i=1:n_clusters
+    for j=1:n_dim_analysis
+        write( file, string(data_train[ cluster_centers[i] , j ]," " ) )
+    end
+    write( file, "\n" )
+end
+close( file )
+
+print("Printing Training Clusters\n")
+for i=1:size(cluster_centers)[1]
+    file=open( string( folder, string("cluster",i,"-",n_dim_analysis,"_daura_",cut_off,".dat") ), "w" )
+    for j=1:n_train
+        if index_cluster[i] == i
+            for k=1:n_dim_analysis
+                if assignments[ i, j ] != 0
+                    write(file,string( data_train[ assignments[ i, j ], k ] ," ") )
+                end
+            end
+            write(file,string("\n"))
+        end
+    end
+    close(file)
+end
