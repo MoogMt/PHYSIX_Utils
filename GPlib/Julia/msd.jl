564941807e4ba4fa65327e97f6bf3e6ad647dbe1
CondensedOtters <moog.mathieu@gmail.com>
2018-09-17 17:41:22 +0200

    modified:   GPlib/Julia/msd.jl

--- GPlib/Julia/msd.jl
+++ GPlib/Julia/msd.jl
@@ -10,52 +10,56 @@ for V in Volumes
 
     for T in Temperatures
 
-
         folder=string("/media/moogmt/Stock/CO2/AIMD/Liquid/",func,"/",V,"/",T,"K/")
         file="TRAJEC.xyz"
 
-        traj = filexyz.read(string(folder,file),stride)
-        cell=cell_mod.Cell_param(V,V,V)
+        if isfile( string(folder,file) )
 
-        nb_steps=size(traj)[1]
-        nb_atoms=size(traj[1].names)[1]
+            traj = filexyz.read(string(folder,file),stride)
+            cell=cell_mod.Cell_param(V,V,V)
 
-        stop_100=20000
+            nb_steps=size(traj)[1]
+            nb_atoms=size(traj[1].names)[1]
 
-        count=0
+            stop_100=20000
 
-        x0_std_block=traj[1].positions
+            count=0
+            msd_stock=0
 
-        file_std=open(string(folder,"MSD.dat"),"w")
-        file_block=open(string(folder,"MSD_block.dat"),"w")
+            x0_std_block=traj[1].positions[:,:]
 
-        for step=1:stop_100
-            print("V = ",V," T = ",T," Progress: ",step/stop_100*100,"% \n")
-            if step > nb_steps
-                break
-            end
-            # WRAP
-            msd_local_std = 0
-            msd_local_block = 0
-            for atom=1:nb_atoms
-                for i=1:3
-                    msd_local_std += (traj[step].positions[atom,i]-traj[1].positions[atom,i])*(traj[step].positions[atom,i]-traj[1].positions[atom,i])
-                    msd_local_block += (traj[step].positions[atom,i]-x0_std_block[atom,i])*(traj[step].positions[atom,i]-x0_std_block[atom,i])
+            file_std=open(string(folder,"MSD.dat"),"w")
+            file_block=open(string(folder,"MSD_block.dat"),"w")
+
+            for step=1:stop_100
+                print("V = ",V," T = ",T," Progress: ",step/stop_100*100,"% \n")
+                if step > nb_steps
+                    break
+                end
+                # WRAP
+                msd_local_std = 0
+                msd_local_block = 0
+                for atom=1:nb_atoms
+                    for i=1:3
+                        msd_local_std += (traj[step].positions[atom,i]-traj[1].positions[atom,i])*(traj[step].positions[atom,i]-traj[1].positions[atom,i])
+                        msd_local_block += (traj[step].positions[atom,i]-x0_std_block[atom,i])*(traj[step].positions[atom,i]-x0_std_block[atom,i])
+                    end
+                end
+                write(file_std,string(step*unit," ",msd_local_std,"\n"))
+                write(file_block,string(step*unit," ",msd_local_block + msd_stock,"\n"))
+
+                count += 1
+                if count == 1000
+                    x0_std_block = traj[step].positions
+                    msd_stock = msd_local_block
+                    count=0
                 end
             end
-            count += 1
-            if count == 1000
-                x0_std_block = traj[step].positions
-                count=0
-            end
-            msd_std +=  msd_local_std
-            msd_block += msd_local_block
-            write(file_std,string(step*unit," ",msd_local_std,"\n"))
-            write(file_block,string(step*unit," ",msd_local_block,"\n"))
-        end
 
-        close(file_std)
-        close(file_block)
+            close(file_std)
+            close(file_block)
+
+        end
 
     end
 
