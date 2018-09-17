cabda7b6ff65b7ab29328ca508fdca48f976734f
CondensedOtters <moog.mathieu@gmail.com>
2018-09-17 18:12:54 +0200

    modified:   GPlib/Julia/msd.jl

--- GPlib/Julia/msd.jl
+++ GPlib/Julia/msd.jl
@@ -1,63 +1,71 @@
 include("contactmatrix.jl")
 
 func="PBE-MT"
-Volumes=[8.82,9.0,9.05,9.1,9.15,9.2,9.25,9.3,9.35,9.375,9.4,9.8,10.0]
+Volumes=[8.82]
 Temperatures=[2000,2250,2500,3000]
-
+stride=1
 unit=0.005
+block_size=[500,1000,2000]
 
-for V in Volumes
+for size_block in block_size
 
-    for T in Temperatures
+    for V in Volumes
 
-        folder=string("/media/moogmt/Stock/CO2/AIMD/Liquid/",func,"/",V,"/",T,"K/")
-        file="TRAJEC.xyz"
+        for T in Temperatures
 
-        if isfile( string(folder,file) )
+            folder=string("/media/moogmt/Stock/CO2/AIMD/Liquid/",func,"/",V,"/",T,"K/")
+            file="TRAJEC.xyz"
 
-            traj = filexyz.read(string(folder,file),stride)
-            cell=cell_mod.Cell_param(V,V,V)
+            if isfile( string(folder,file) )
 
-            nb_steps=size(traj)[1]
-            nb_atoms=size(traj[1].names)[1]
+                traj = filexyz.read(string(folder,file),stride)
+                cell=cell_mod.Cell_param(V,V,V)
 
-            stop_100=20000
+                nb_steps=size(traj)[1]
+                nb_atoms=size(traj[1].names)[1]
 
-            count=0
-            msd_stock=0
+                stop_100=20000
 
-            x0_std_block=traj[1].positions[:,:]
+                count=0
+                msd_stock=0
 
-            file_std=open(string(folder,"MSD.dat"),"w")
-            file_block=open(string(folder,"MSD_block.dat"),"w")
+                x0_std_block=traj[1].positions[:,:]
 
-            for step=1:stop_100
-                print("V = ",V," T = ",T," Progress: ",step/stop_100*100,"% \n")
-                if step > nb_steps
-                    break
-                end
-                # WRAP
-                msd_local_std = 0
-                msd_local_block = 0
-                for atom=1:nb_atoms
-                    for i=1:3
-                        msd_local_std += (traj[step].positions[atom,i]-traj[1].positions[atom,i])*(traj[step].positions[atom,i]-traj[1].positions[atom,i])
-                        msd_local_block += (traj[step].positions[atom,i]-x0_std_block[atom,i])*(traj[step].positions[atom,i]-x0_std_block[atom,i])
+                file_std=open(string(folder,"MSD.dat"),"w")
+                file_block=open(string(folder,"MSD_block-",size_block,".dat"),"w")
+
+                for step=1:stop_100
+                    print("V = ",V," T = ",T," Progress: ",step/stop_100*100,"% \n")
+                    if step > nb_steps
+                        break
                     end
-                end
-                write(file_std,string(step*unit," ",msd_local_std,"\n"))
-                write(file_block,string(step*unit," ",msd_local_block + msd_stock,"\n"))
+                    # WRAP
+                    msd_local_std = 0
+                    msd_local_block = 0
+                    for atom=1:nb_atoms
+                        for i=1:3
+                            msd_local_std += (traj[step].positions[atom,i]-traj[1].positions[atom,i])*(traj[step].positions[atom,i]-traj[1].positions[atom,i])
+                            msd_local_block += (traj[step].positions[atom,i]-x0_std_block[atom,i])*(traj[step].positions[atom,i]-x0_std_block[atom,i])
+                        end
+                    end
+                    write(file_std,string(step*unit," ",msd_local_std/nb_atoms,"\n"))
+                    msd_local_block = msd_local_block/nb_atoms+ msd_stock
+                    write(file_block,string(step*unit," ",msd_local_block,"\n"))
 
-                count += 1
-                if count == 1000
-                    x0_std_block = traj[step].positions
-                    msd_stock = msd_local_block
-                    count=0
+
+                    if count == 1000
+                        x0_std_block = traj[step].positions
+                        msd_stock = msd_local_block
+                        count=0
+                    end
+
+                    count += 1
                 end
+
+                close(file_std)
+                close(file_block)
+
             end
-
-            close(file_std)
-            close(file_block)
 
         end
 
