959372ae7fcc6c516257eb5391c60b2a9ffb1e89
CondensedOtters <moog.mathieu@gmail.com>
2018-09-28 16:48:32 +0200

    modified:   GPlib/Julia/CO2_coordinance.jl

--- GPlib/Julia/CO2_coordinance.jl
+++ GPlib/Julia/CO2_coordinance.jl
@@ -1,9 +1,9 @@
 # Loading file
 include("contactmatrix.jl")
 
-func="PBE-MT"
+func = "PBE-MT"
 
-Temperatures=[ 2000, 2250, 25000, 3000 ]
+Temperatures = [ 2000, 2250, 25000, 3000 ]
 Volumes = [ 8.6, 8.82, 9.0, 9.05, 9.1, 9.15, 9.2, 9.25, 9.3, 9.325, 9.35, 9.375, 9.4, 9.5, 9.8 ]
 Cut_Off = [ 1.6 , 1.7 , 1.75, 1.8 ]
 
@@ -12,29 +12,32 @@ nbO = 64
 
 unit=0.005
 
+start_step=0
+stride = 1
+
 for cut_off in Cut_Off
     for V in Volumes
         for T in Temperatures
 
-            folder=string("/media/moogmt/Stock/CO2/AIMD/Liquid/",func,"/",V,"/",T,"/")
+            folder=string("/media/moogmt/Stock/CO2/AIMD/Liquid/",func,"/",V,"/",T,"K/")
             file=string(folder,"TRAJEC_wrapped.xyz")
 
-            if ! isfile(string(folder,file))
+            if ! isfile(file)
                 break
             end
 
-            atoms = filexyz.readFastFile(file)
+            traj = filexyz.read( file, stride, start_step )
             cell=cell_mod.Cell_param(V,V,V)
 
-            nb_steps=size(atoms)[1]
-            nb_atoms=size(atoms[1].names)[1]
+            nb_steps=size(traj)[1]
+            nb_atoms=size(traj[1].names)[1]
 
             file_C = open(string(folder,"C_coordinance.dat"),"w")
             file_O = open(string(folder,"O_coordinance.dat"),"w")
 
             for step=1:nb_steps
 
-                print("Progress: ",step/nb_steps*100," %\n")
+                print("Currently at - ",V," - ",T,"-",cut_off," ; progress:",step/nb_steps*100," %\n")
 
                 write(file_C,string(step*unit," "))
                 coordC_avg=0
@@ -42,7 +45,7 @@ for cut_off in Cut_Off
                     coord_C=0
                     for atom=1:nb_atoms
                         if carbon != atom
-                            if dist=cell_mod.distance(atoms[k],cell,carbon,atom) < cut_off
+                            if cell_mod.distance(traj[step],cell,carbon,atom) < cut_off
                                 coord_C += 1
                             end
                         end
@@ -59,7 +62,7 @@ for cut_off in Cut_Off
                     coord_O = 0
                     for atom=1:nb_atoms
                         if oxygen+nbC != atom
-                            if  dist=cell_mod.distance(atoms[k],cell,carbon,atom) < cut_off
+                            if  cell_mod.distance(traj[step],cell,oxygen,atom) < cut_off
                                 coord_O += 1
                             end
                         end
