0f152b70ab84200d6b72028afa8369667e79f1b8
CondensedOtters <moog.mathieu@gmail.com>
2018-11-26 16:47:55 +0100

    modified:   CO2_analysis/advanced_coord.jl

--- CO2_analysis/advanced_coord.jl
+++ CO2_analysis/advanced_coord.jl
@@ -6,7 +6,7 @@ include(string(GPfolder,"contactmatrix.jl"))
 folder_base="/media/moogmt/Stock/CO2/AIMD/Liquid/PBE-MT/"
 
 # Thermo data
-Volumes=[9.0]
+Volumes=[8.82]
 Temperatures=[3000]
 Cut_Off=[1.75]
 
@@ -40,14 +40,14 @@ file_out=open(file_coordinance,"w")
 
 bond_matrix=zeros(nb_atoms,nb_atoms)
 
-step=1
+step_sim=1
 
 nb_type=2
 
 bond_matrix=zeros(nb_atoms,nb_atoms)
 for atom1=1:nb_atoms
     for atom2=atom1+1:nb_atoms
-        if cell_mod.distance(traj[step],cell,atom1,atom2) < cut_off
+        if cell_mod.distance(traj[step_sim],cell,atom1,atom2) < cut_off
             bond_matrix[atom1,atom2]=1
             bond_matrix[atom2,atom1]=1
         end
@@ -55,30 +55,30 @@ for atom1=1:nb_atoms
 end
 
 for carbon=1:nbC
-    coord_type=zeros(2)
-    coord_nb=zeros(5,2)
-    index_nb=zeros(5,2)
+    coord_type=zeros(Int,2)
+    coord_nb=zeros(Int,5,2)
+    index_nb=zeros(Int,5,2)
     count=0
     for atom=1:nb_atoms
-        if carbon=atom
+        if carbon == atom
             continue
         end
         if bond_matrix[carbon,atom] > 0
             check=0
             if atom < 32
-                check=1
+                check += 1
             else
-                check=2
+                check += 2
             end
             coord_type[check] += 1
             coord_nb[check] = sum( bond_matrix[atom,:] )
             index_nb[coord_type[check],check]= atom
         end
         if sum(coord_type) > max_coord
-            print("Oupsie: step-",step," carbon-",carbon," atom-",atom,"\n")
+            print("Oupsie: step-",step_sim," carbon-",carbon," atom-",atom,"\n")
         end
     end
-    write(file_coordinance,string(step," ",carbon," "))
+    write(file_coordinance,string(step_sim," ",carbon," "))
     for type=1:nb_type
         write(file_coordinance,string(coord_type[type]," "))
         for i=1:5
@@ -88,4 +88,4 @@ for carbon=1:nbC
     write(file_coordinance,string("\n"))
 end
 
-close(file_coordinance)
+close(file_out)
