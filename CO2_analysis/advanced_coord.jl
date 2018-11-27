5faf11655565923b942453bff9d928c6ef583733
MoogMt <moog.mathieu@gmail.com>
2018-11-27 01:52:46 +0100

    modified:   CO2_analysis/advanced_coord.jl

--- CO2_analysis/advanced_coord.jl
+++ CO2_analysis/advanced_coord.jl
@@ -141,7 +141,7 @@ for lag=1:size(case_transition)[3]
             # Specific i-j transition
             global p_truth=case_transition[i,j,lag]
             global p_test=0
-            for k=1:size(case_transition[1])
+            for k=1:size(case_transition)[1]
                 if k != j
                     p_test += case_transition[i,k,lag]*case_transition[k,j,lag]
                 end
