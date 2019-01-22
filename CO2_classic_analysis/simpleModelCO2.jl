d73c0d8d1d9723d82b43bdcdca80eb30c37e0de6
CondensedOtters <moog.mathieu@gmail.com>
2019-01-22 21:44:00 +0100

    new file:   CO2_classic_analysis/simpleModelCO2.jl 	new file:   MoS2_analysis/test_clustering.jl 	new file:   MoS2_analysis/test_clustering2.jl

---
+++ CO2_classic_analysis/simpleModelCO2.jl
@@ -0,0 +1,100 @@
+
+# Laio Algo
+GPfolder=string("/home/moogmt/PHYSIX_Utils/GPlib/Julia/")
+
+include(string(GPfolder,"xyz.jl"))
+include(string(GPfolder,"cell.jl"))
+include(string(GPfolder,"pdb.jl"))
+
+# Reading PDB file
+folder="/media/moogmt/Stock/CO2/Structures/Pa3/SuperFF/"
+molecules, cell = pdb.readStep( string(folder,"Pa3-super.pdb") )
+
+# C-O cut-off to build molecules
+cut_off=1.75
+
+#--------------------
+# Building molecules
+#------------------------------------------------------------------------------
+count_mol=1
+nb_atoms=size(molecules.atom_names)[1]
+for atom1=1:nb_atoms
+    if molecules.atom_names[atom1] == "C"
+        molecules.mol_index[atom1]=count_mol
+        molecules.mol_names[atom1]="CO2"
+        for atom2=1:nb_atoms
+            if atom1 == atom2
+                continue
+            end
+            dist=cell_mod.distance(molecules,cell,atom1,atom2)
+            if dist < cut_off
+                #Adding oxygen to molecule
+                molecules.atom_names[atom2]=string("O") # Changing name to O1 or O2
+                molecules.mol_names[atom2]="CO2"        # Changing molecule name
+                molecules.mol_index[atom2]=count_mol    # Putting index of molecule
+            end
+        end
+        global count_mol+=1
+    end
+end
+#-------------------------------------------------------------------------------
+
+
+#--------------------------------------
+# Sorting atom_list by molecule index
+#-------------------------------------------------------------------------------
+for i=1:nb_atoms-1
+    for j=i+1:nb_atoms
+        if molecules.mol_index[i] < molecules.mol_index[j]
+            # Storing
+            atom_mod.switchAtoms(molecules,i,j)
+        end
+    end
+end
+for i=1:nb_atoms
+    molecules.atom_index[i] = i
+end
+#-------------------------------------------------------------------------------
+
+# Moving all O atoms to 1.16 of the C
+target_distance=1.163
+for oxygen=1:nb_atoms
+    if molecules.atom_names[oxygen] == "O"
+        for carbon=1:nb_atoms
+            if molecules.atom_names[carbon] == "C"
+                actual_distance=cell_mod.distance(molecules,cell,carbon,oxygen)
+                if actual_distance < cut_off
+                    ratio=target_distance/actual_distance
+                    for i=1:3
+                        molecules.positions[oxygen,i] = molecules.positions[carbon,i]+(molecules.positions[oxygen,i]-molecules.positions[carbon,i])*ratio
+                    end
+                end
+            end
+        end
+    end
+end
+
+
+# Moving all O atoms to 1.16 of the C
+for oxygen=1:nb_atoms
+    if molecules.atom_names[oxygen] == "O"
+        for carbon=oxygen+1:nb_atoms
+            if molecules.atom_names[carbon] == "C"
+                if cell_mod.distance(molecules,cell,carbon,oxygen) < 1.75
+                    print("distance: ",cell_mod.distance(molecules,cell,carbon,oxygen),"\n")
+                end
+            end
+        end
+    end
+end
+
+
+#-----------------
+# Writting PDB
+#------------------------------------------------
+pdb.writeStep(molecules,cell,"/home/moogmt/Pa3_ready.pdb")
+#------------------------------------------------
+
+cell_mod.wrap(molecules,cell)
+
+pdb.writeStep(molecules,cell,"/home/moogmt/Pa3_ready_wrapped.pdb")
