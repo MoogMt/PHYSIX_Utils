8eb0b3a78e1c483317352f85b118756ed14bcd2b
CondensedOtters <moog.mathieu@gmail.com>
2019-02-21 11:31:03 +0100

    modified:   CO2_analysis/test_prebio-markov.jl

--- CO2_analysis/test_prebio-markov.jl
+++ CO2_analysis/test_prebio-markov.jl
@@ -24,35 +24,39 @@ unit=0.005
 # Volume of the cell (only orthorombic is implemented yet)
 V=16.36074
 
-# for V in Volumes
-#     for T in Temperatures
-
+# Defining base folder
 folder_in=string("/media/moogmt/Stock/Theo_equilibration/")
+
+# File of trajectory to be read
 file=string(folder_in,"01_equilibration.xyz")
 
+# Folder where to put the out data
 folder_out=string(folder_in,"Data/")
 
+# Reading xyz
 print("Computing Data\n")
 traj=filexyz.readFastFile(file)
 cell=cell_mod.Cell_param(V,V,V)
 
+# Computing neighbours
 data,types,type_atoms=buildCoordinationMatrix( traj , cell , cut_off_bond, max_neigh )
 nb_types=size(types)[1]
 
+# Creating states and assigning data
 states, percent, state_matrix, type_states = assignDataToStates( data, nb_types, type_atoms )
 writeStates(string(folder_out,"markov_initial_states.dat"),states,percent,types,type_states)
 writeStateMatrix( string(folder_out,"initial_state_matrix.dat"), state_matrix )
 
+# Isolating only pertinent states for transition study
 states, type_states = isolateSignificantStates( states, percent, cut_off_states, type_states )
 state_matrix, percent = assignDataToStates( data , states , nb_types , type_states, type_atoms, false)
 writeStates(string(folder_out,"markov_final_states-",cut_off_states,".dat"),states,percent,types,type_states)
 writeStateMatrix( string(folder_out,"final_state_matrix.dat"), state_matrix )
 
+# Transition matrix study
 transitions_matrix=transitionMatrix(states,state_matrix,type_states,nb_types,type_atoms,min_lag,max_lag,d_lag)
-transitions_matrix_CK=[transitions_matrix[1]]
-for i=1:nb_types
-    chappmanKormologov( transition_matrix[1] )
+# test of Chappman Kolmogorov test (Markov Testing not necessary if one just wants to study the transitions)
+transitions_matrix_CK=[chappmanKormologov( transition_matrix[1])]
+for i=2:nb_types
+    push!( transition_matrix_CK, chappmanKormologov( transition_matrix[i] ) )
 end
-
-# end
-# end
