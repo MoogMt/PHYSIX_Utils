e0036819f4596b58185b7c7faf80e74c653198b6
CondensedOtters <moog.mathieu@gmail.com>
2019-07-17 13:18:30 +0200

    modified:   ELF/elf_glycine.jl

--- ELF/elf_glycine.jl
+++ ELF/elf_glycine.jl
@@ -1,23 +1,21 @@
 GPfolder=string("/home/moogmt/PHYSIX_Utils/GPlib/Julia/")
-CO2folder=string("/home/moogmt/PHYSIX_Utils/CO2_analysis/")
+push!(LOAD_PATH, GPfolder)
 
-include(string(GPfolder,"geom.jl"))
-include(string(GPfolder,"cubefile.jl"))
-include(string(GPfolder,"clustering.jl"))
-
-include(string(CO2folder,"markovCO2.jl"))
+using atom_mod
+using cell_mod
+using cube_mod
 
 folder_base="/media/moogmt/Stock/Andrea/Glycine_TS_ELF/"
 
 nb_steps=1091
 
 
-nb_box_distance=200
-cut_off_distance=3
-min_distance=0.9
+nb_box_distance=50
+cut_off_distance=5
+min_distance=0.2
 delta_distance=(cut_off_distance-min_distance)/nb_box_distance
 
-nb_box_elf=50
+nb_box_elf=20
 delta_elf=1/nb_box_elf
 
 hist2d=zeros(Int,nb_box_distance,nb_box_elf)
@@ -34,8 +32,6 @@ nbN=1
 
 nb_atoms=nbC+nbO+nbN+nbH
 
-atoms=Main.atom_mod.AtomList(nb_atoms)
-
 for step=1:10:nb_steps
 	print("Progress: ",step/nb_steps*100,"%\n")
 	atoms, cell_matrix, elf = cube_mod.readCube( string(folder_base,"ELF_shoot1_",step,".cube") )
@@ -43,12 +39,11 @@ for step=1:10:nb_steps
 	atoms.positions=cell_mod.wrap(atoms.positions,cell)
 
 	for atom1=start_H:start_H+nbH
-		for atom2=start_O:start_O+nbO
+		for atom2=start_N:start_N+nbN
 			distance=cell_mod.distance(atoms.positions,cell, atom1 , atom2)
 			if distance < cut_off_distance && min_distance < distance
-				index_ = cube_mod.dataInTheMiddleWME( atoms, cell , atom1, atom2, elf )
-				elf_value=elf.matrix[index_[1]+1,index_[2]+1,index_[3]+1]
-				nx=Int(trunc(distance/delta_distance))+1
+				elf_value = cube_mod.dataInTheMiddleWME( atoms, cell , atom1, atom2, elf )
+				nx=Int(trunc((distance-min_distance)/delta_distance))+1
 				ny=Int(trunc(elf_value/delta_elf))+1
 				hist2d[nx,ny] += 1
 			end
@@ -57,11 +52,11 @@ for step=1:10:nb_steps
 
 end
 
-file_out=open(string(folder_base,"histOH.dat"),"w")
+file_out=open(string(folder_base,"histNH.dat"),"w")
 for i=1:nb_box_distance
 	for j=1:nb_box_elf
-		write(i*delta_distance+min_distance," ",j*delta_elf," ",hist2d[i,j],"\n")
+		write(file_out,string(i*delta_distance+min_distance," ",j*delta_elf," ",hist2d[i,j],"\n"))
 	end
-	write("\n")
+	write(file_out,string("\n"))
 end
 close(file_out)
