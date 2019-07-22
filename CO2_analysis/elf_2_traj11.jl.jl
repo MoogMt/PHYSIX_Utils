e877332fa8c33145994c2bc9f127ac7c6d5fc9d5
CondensedOtters <moog.mathieu@gmail.com>
2019-07-22 17:41:12 +0200

    new file:   CO2_analysis/elf_2_traj10.jl.jl 	new file:   CO2_analysis/elf_2_traj11.jl.jl 	new file:   CO2_analysis/elf_2_traj3.jl 	new file:   CO2_analysis/elf_2_traj4.jl 	new file:   CO2_analysis/elf_2_traj5.jl 	new file:   CO2_analysis/elf_2_traj6.jl 	new file:   CO2_analysis/elf_2_traj7.jl 	new file:   CO2_analysis/elf_2_traj8.jl 	new file:   CO2_analysis/elf_2_traj9.jl

---
+++ CO2_analysis/elf_2_traj11.jl.jl
@@ -0,0 +1,67 @@
+GPfolder=string("/home/moogmt/PHYSIX_Utils/GPlib/Julia/")
+push!(LOAD_PATH, GPfolder)
+
+using atom_mod
+using cell_mod
+using cube_mod
+using clustering
+
+folder_base="/media/moogmt/Stock/Andrea/Glycine_TS_ELF/"
+
+start_=1
+stride_=10
+nb_steps=1351
+
+start_C=1
+nbC=2
+start_O=3
+nbO=80
+start_H=5
+nbH=164
+start_N=84
+nbN=1
+
+nb_atoms=nbC+nbO+nbN+nbH
+
+
+file_out=open(string(folder_base,"TRAJEC_wrapped.xyz"),"w")
+for step=start_:stride_:nb_steps
+	print("Progress: ",step/nb_steps*100,"%\n")
+	if ! isfile( string(folder_base,"ELF_shoot2_"step,".cube") )
+		continue
+	end
+	atoms, cell_matrix, elf = cube_mod.readCube( string(folder_base,"ELF_shoot2_"step,".cube") )
+	cell=cell_mod.Cell_param(cell_mod.cellMatrix2Params(cell_matrix))
+	atoms.positions=cell_mod.wrap(atoms.positions,cell)
+	write(file_out,string(nb_atoms,"\n"))
+	write(file_out,string("STEP ",step,"\n"))
+	for carbon=1:nbC
+		write(file_out,string("C "))
+		for i=1:3
+			write(file_out,string(atoms.positions[carbon,i]," "))
+		end
+		write(file_out,string("\n"))
+	end
+	for oxygen=nbC+1:nbC+nbO
+		write(file_out,string("O "))
+		for i=1:3
+			write(file_out,string(atoms.positions[oxygen,i]," "))
+		end
+		write(file_out,string("\n"))
+	end
+	for hydrogen=nbC+nbO+1:nbC+nbO+nbH
+		write(file_out,string("O "))
+		for i=1:3
+			write(file_out,string(atoms.positions[hydrogen,i]," "))
+		end
+		write(file_out,string("\n"))
+	end
+	for nitrogen=nbC+nbO+nbH+1:nbC+nbO+nbH+nbN
+		write(file_out,string("O "))
+		for i=1:3
+			write(file_out,string(atoms.positions[nitrogenn,i]," "))
+		end
+		write(file_out,string("\n"))
+	end
+end
+close(file_out)
