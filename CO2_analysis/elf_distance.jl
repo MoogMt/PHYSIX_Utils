126d8b89df9c2cf35feeda860ce95b73a5a718ee
CondensedOtters <moog.mathieu@gmail.com>
2019-07-23 15:33:29 +0200

    modified:   CO2_analysis/elf_distance.jl

--- CO2_analysis/elf_distance.jl
+++ CO2_analysis/elf_distance.jl
@@ -1,19 +1,59 @@
-include("atoms.jl")
-include("cell.jl")
-include("cubefile.jl")
+GPfolder=string("/home/moogmt/PHYSIX_Utils/GPlib/Julia/")
+push!(LOAD_PATH, GPfolder)
 
-step_max=1001
-step_min=1
-d_step=1
+using atom_mod
+using cell_mod
+using cube_mod
+using clustering
 
-distance_data=[]
-elf_data=[]
-used=[]
+folder_base="/media/moogmt/Stock/Mathieu/CO2/AIMD/Liquid/PBE-MT/ELF/8.82/Trajectory_2/"
 
-for step=step_min:d_step:step_max
 
-    #------------------------------------------------------------------------------
-    atoms, cell1, ELF1 = cube_mod.readCube(string("/media/moogmt/Stock/cube_TS14_gly/ELF_shoot2_",step,".cube"))
-    #---------------
+start_=1
+stride_=1
+nb_steps=100
 
+start_C=1
+nbC=32
+start_O=32
+nbO=64
+
+nb_atoms=nbC+nbO
+
+nb_box=50
+nb_elf=50
+
+hist2D=zeros(Real,nb_box,nb_elf)
+
+cut_off_distance=2.8
+min_distance=1.0
+
+delta_distance=(cut_off_distance-min_distance)/nb_box
+delta_elf=1/nb_elf
+
+for step=start_:stride_:nb_steps
+    print("Progress: ",step/nb_steps*100,"%\n")
+	atoms, cell_matrix, elf = cube_mod.readCube( string(folder_base,step,"_elf.cube") )
+	for carbon=start_C:start_C+nbC-1
+		for oxygen=start_O:startO+nbO-1
+			distance=cell_mod.distance( atoms.positions, cell, carbon , oxygen )
+			elf=dataInTheMiddleWME( atoms, cell , carbon, oxygen, elf )
+			nx=Int(trunc(elf/delta_elf)+1)
+			ny=Int(trunc((distance-min_distance)/delta_distance)+1)
+			hist2D[nx,ny] += 1
+		end
+	end
 end
+
+for i=1:nb_box
+	hist2D[i,:] /= sum(hist2D[i,:])
+end
+
+file_out=open(string(folder_base,"elf_distance-",nb_steps,".dat"),"w")
+for i=1:nb_box
+	for j=1:nb_elf
+		write(file_out,string(i*delta_distance+min_distance," ",j*delta_elf," ",hist2D[i,j],"\n"))
+	end
+	write(file_out,string("\n"))
+end
+close(file_out)
