9d560e5624e5ef4d064c092450408eea27597751
MoogMt <moog.mathieu@gmail.com>
2019-08-18 05:47:17 +0200

    modified:   GPlib/Julia/cell_mod.jl

--- GPlib/Julia/cell_mod.jl
+++ GPlib/Julia/cell_mod.jl
@@ -87,7 +87,7 @@ end
 Cell=Union{Cell_param, Cell_vec, Cell_matrix}
 #---------------------------------------------
 
-# Conversion
+# Conversions
 #-------------------------------------------------------------------------------
 function cellMatrix2Params( cell_matrix::Array{T1,2} )  where { T1 <: Real }
     params=zeros(Real,3)
@@ -219,7 +219,10 @@ end
 #-------------------------------------------------------------------------------
 
 #-------------------------------------------------------------------------------
-function scaleVector( vector::Vector{T1}, cell_matrix::Array{T2,2} ) where { T1 <: Real , T2 <: Real }
+function invertCell( cell_matrix::Array{T1,2} ) where { T1 < : Real }
+    return LinearAlgebra.inv(cell_matrix)
+end
+function getScaledVector( vector::Vector{T1}, cell_matrix::Array{T2,2} ) where { T1 <: Real , T2 <: Real }
     if LinearAlgebra.det(cell_matrix) == 0
         print("MATRIX NOT INVERSIBLE!")
         return 0
@@ -233,8 +236,57 @@ function scaleVector( vector::Vector{T1}, cell_matrix::Array{T2,2} ) where { T1 
     end
     return vector2
 end
+function getScaledVector( vector::Vector{T1}, cell_matrix::T2 ) where { T1 <: Real , T2 <: Cell_matrix }
+    return getScaledVector(vector,cell_matrix.matrix)
+end
+function getScaleVectorInv( vector::Vector{T1}, inv_cell_matrix::Array{T2,2} ) where { T1 <: Real, T2 <: Real}
+    vector2=zeros(3)
+    for i=1:3
+        for j=1:3
+            vector2[i] += cell_mat_inverse[i,j]*vector[j]
+        end
+    end
+    return vector2
+end
+function scaleVector( vector::Vector{T1}, cell_matrix::Array{T2,2} ) where { T1 <: Real , T2 <: Real }
+    if LinearAlgebra.det(cell_matrix) == 0
+        print("MATRIX NOT INVERSIBLE!")
+        return 0
+    end
+    cell_mat_inverse=LinearAlgebra.inv(cell_matrix)
+    for i=1:3
+        for j=1:3
+            vector[i] += cell_mat_inverse[i,j]*vector[j]
+        end
+    end
+    return
+end
 function scaleVector( vector::Vector{T1}, cell_matrix::T2 ) where { T1 <: Real , T2 <: Cell_matrix }
     return scaleVector(vector,cell_matrix.matrix)
+end
+function getScaleMatrix( target_matrix::Array{T1,2}, cell_matrix::Array{T2,2} ) where { T1 <: Real, T2 <: Real }
+    inv_cell_matrix=LinearAlgebra.inv(cell_matrix)
+    size_matrix=size(target_matrix)[1]
+    scaled_matrix=copy(target_matrix)
+    for i=1:size_matrix
+        scaleVector(scaled_matrix[i,:],cell_matrix)
+    end
+    return scaled_matrix
+end
+function getScaleMatrix( target_matrix::Array{T1,2}, cell_matrix::T2 ) where { T1 <: Real, T2 <: Cell_matrix }
+    return getScaleMatrix(target_matrix,cell_matrix.matrix)
+end
+function deScaleVector( vector::Vector{T1}, cell_matrix::Array{T2,2}  ) where { T1 <: Real , T2 <: Real }
+    vector2=zeros(3)
+    for i=1:3
+        for j=1:3
+            vector2[i] += cell_matrix[i,j]*vector[j]
+        end
+    end
+    return
+end
+function deScaleVector( vector::Vector{T1}, cell_matrix::T2 ) where { T1 <: Real , T2 <: Cell_matrix }
+    return deScaleVector(vector,cell_matrix.matrix)
 end
 #-------------------------------------------------------------------------------
 
@@ -258,22 +310,30 @@ function distance( v1::Vector{T1}, v2::Vector{T2}, cell::Vector{T3} ) where { T1
     return sqrt(dist)
 end
 function distance( v1::Vector{T1}, v2::Vector{T2}, cell_matrix::Array{T3,2} ) where { T1 <: Real, T2 <: Real, T3 <: Real }
-    scaled_v1=scaleVector(v1,cell_matrix)
-    scaled_v2=scaleVector(v2,cell_matrix)
+    v1_scaled=getScaledVector(v1,cell_matrix)
+    v2_scaled=getScaledVector(v2,cell_matrix)
     ds=zeros(3)
-    # Min Image Convention
+    # Distance + Min Image Convention
     for i=1:3
-        ds[i] = scaled_v1[i]-scaled_v2[i]
+        ds[i] = v1_scaled[i]-v2_scaled[i]
         ds[i] = ds[i] - round(ds[i])
     end
-    # Descaling
-    vector=zeros(3)
+    # Descaling of distance vector
+    deScaleVector(ds,cell_matrix)
+    return sqrt(dot(ds,ds))
+end
+function distance( v1::Vector{T1}, v2::Vector{T2}, cell_matrix::Array{T3,2}, inv_cell_matrix::Array{T4,2} ) where { T1 <: Real, T2 <: Real, T3 <: Real, T4 <: Real }
+    v1_scaled=getDeScaleVectorInv(v1,inv_cell_matrix)
+    v2_scaled=getDeScaleVectorInv(v2,inv_cell_matrix)
+    ds=zeros(3)
+    # Distance + Min Image Convention
     for i=1:3
-        for j=1:3
-            vector[i] += cell_matrix[i,j]*ds[j]
-        end
+        ds[i] = v1_scaled[i]-v2_scaled[i]
+        ds[i] = ds[i] - round(ds[i])
     end
-    return sqrt(dot(vector,vector))
+    # Descaling of distance vector
+    scaleVector(ds,cell_matrix)
+    return sqrt(dot(ds,ds))
 end
 function distance( v1::Vector{T1}, v2::Vector{T2}, cell_matrix::T3 ) where { T1 <: Real, T2 <: Real, T3 <: Cell_matrix }
     return distance(v1,v2,cell_matrix.matrix)
