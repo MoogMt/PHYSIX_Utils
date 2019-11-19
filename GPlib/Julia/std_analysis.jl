2486f9265672df392bb886ff71a555de73537ab6
MoogMt <moog.mathieu@gmail.com>
2019-11-19 22:21:28 +0100

    modified:   GPlib/Julia/std_analysis.jl 	modified:   Projects/CO2_analysis/Checks/congruence_test.jl

--- GPlib/Julia/std_analysis.jl
+++ GPlib/Julia/std_analysis.jl
@@ -5,7 +5,7 @@ using cell_mod
 
 function computeRMSD( structure1::T1, structure2::T2 ) where { T1 <: atom_mod.AtomList, T2 <: AtomList }
     nb_atoms1=size(structure1.names)[1]
-    nb_atoms2=size(structure2.names)[2]
+    nb_atoms2=size(structure2.names)[1]
     if nb_atoms1 != nb_atoms2
         print("Comparing two structures with different number of atoms. Stopping now!")
         return -1
