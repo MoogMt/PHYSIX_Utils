4338390b2dfd48c8c72bc2273e4166f2cbe49bfd
MoogMt <moog.mathieu@gmail.com>
2019-11-19 15:44:41 +0100

    new file:   Projects/CO2_analysis/Checks/congruence_test.jl

---
+++ Projects/CO2_analysis/Checks/congruence_test.jl
@@ -0,0 +1,13 @@
+GPfolder=string("/home/moogmt/PHYSIX_Utils/GPlib/Julia/")
+push!(LOAD_PATH, GPfolder)
+
+using atom_mod
+using cell_mod
+using cube_mod
+using clustering
+using filexyz
+using pdb
+using markov
+using fftw
+using correlation
+using conversion
