dcc56b74b41874e1942124adc02fe600a57b0e4d
MoogMt <moog.mathieu@gmail.com>
2019-11-19 22:01:19 +0100

    new file:   GPlib/Julia/std_analysis.jl

---
+++ GPlib/Julia/std_analysis.jl
@@ -0,0 +1,23 @@
+module std_analysis
+
+using atom_mod
+using cell_mod
+
+function rmsd( structure1::T1, structure2::T2 ) where { T1 <: atom_mod.AtomList, T2 <: AtomList }
+    rmsd=0
+    nb_atoms1=size(structure1.names)[1]
+    nb_atoms2=size(structure2.names)[2]
+    if nb_atoms1 != nb_atoms2
+        print("Comparing two structures with different number of atoms. Stopping now!")
+        return -1
+    end
+    for atom=1:nb_atoms1
+        for i=1:3
+            dist=structure1.positions[atom,i]-structure2.positions[atom,i]
+            rmsd += dist*dist
+        end
+    end
+    return rmsd
+end
+
+end
