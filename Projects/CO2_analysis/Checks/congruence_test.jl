774293bc83deadf9f80a4b15344013ee99ed9a81
MoogMt <moog.mathieu@gmail.com>
2019-11-19 21:12:05 +0100

    modified:   Projects/CO2_analysis/Checks/congruence_test.jl

--- Projects/CO2_analysis/Checks/congruence_test.jl
+++ Projects/CO2_analysis/Checks/congruence_test.jl
@@ -33,6 +33,12 @@ stride_traj=cpmd.readIntputStrideTraj(file_input)
 file_stress=string(folder_in,"STRESS")
 stress,test=cpmd.readStress(file_stress)
 pressure=press_stress.computePressure(stress)
+size_pressure=size(pressure)[1]
 
 file_energy=string(folder_in,"ENERGIES")
 temperature, e_pot,e_tot,msd,comp_time,test=cpmd.readEnergyFile( file_energy )
+size_energy=size(temperature)[1]
+
+file_traj=string(folder_in,"TRAJEC.xyz")
+traj,test=filexyz.readFastFile(file_traj)
+size_traj=size(traj)[1]
