e1a20b9036a8838020091c22f83a7644f1bc4b33
MoogMt <moog.mathieu@gmail.com>
2019-11-20 15:28:03 +0100

    modified:   GPlib/Julia/press_stress.jl

--- GPlib/Julia/press_stress.jl
+++ GPlib/Julia/press_stress.jl
@@ -17,4 +17,17 @@ function computePressure( stress_tensor_matrix::Array{T1,3} ) where { T1 <: Real
     return pressure
 end
 
+function diagStressTensor( stress_tensor::Array{T1,2} ) where { T1 <: Real }
+    return eigvals( stress_tensor )
+end
+
+function diagStressTensor( stress_tensor_matrix::Array{T1,3} ) where { T1 <: Real }
+    nb_step=size(stress_tensor_matrix)[1]
+    vals=zeros(nb_step,3)
+    for step=1:nb_step
+        vals[i,:] = diagStressTensor(stress_tensor_matrix[i,:,:])
+    end
+    return vals
+end
+
 end
