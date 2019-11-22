37e12a6842bc2f4d03422831e0f1cf269d188a2c
MoogMt <moog.mathieu@gmail.com>
2019-11-22 13:34:56 +0100

    modified:   Projects/CO2_analysis/Exp/vdos_compute.jl

--- Projects/CO2_analysis/Exp/vdos_compute.jl
+++ Projects/CO2_analysis/Exp/vdos_compute.jl
@@ -19,13 +19,17 @@ using exp_data
 #folder_base="/media/moogmt/Stock/Mathieu/CO2/AIMD/Liquid/PBE-MT/"
 folder_base="/home/moogmt/Data/CO2/CO2_AIMD/"
 
-Temperatures=[2500]
-Volumes=[8.82]
+Volumes=[9.8]
+Temperatures=[3000]
 
-time_step=0.001
+cpmd_stride=40
+time_step=0.001*conversion.hatime2fs*cpmd_stride
 stride_sim=5
 dt=time_step*stride_sim
-dx=0.1 #Angstrom to nm
+
+max_lag_frac=0.5
+nb_windows=10
+
 
 for V in Volumes
     for T in Temperatures
@@ -34,14 +38,12 @@ for V in Volumes
 
         folder_in=string(folder_base,V,"/",T,"K/")
         file_in=string(folder_in,"TRAJEC.xyz")
-
-        max_lag_frac=0.5
-        to_nm=1
-
+        
         folder_out=string(folder_in,"Data/")
-        file_out=string(folder_out,"vdos-",max_lag_frac,".dat")
+        file_out=string(folder_out,"vdos-frac_",max_lag_frac,"-nbwin_",nb_windows,".dat")
 
-        freq,vdos,test=exp_data.vdosFromPosition( file_in, file_out, max_lag_frac, to_nm, dt )
+        freq,vdos,test=exp_data.vdosFromPosition( file_in, file_out, max_lag_frac, dt, nb_windows )
 
+        print(size(vdos)[1],"\n")
     end
 end
