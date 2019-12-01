ed707d2e4ff1c78676185cdf0f454ebbaa7da65d
MoogMt <moog.mathieu@gmail.com>
2019-12-01 17:43:25 +0100

    new file:   ../CO2_Liquid/testing_code.jl

---
+++ Projects/CO2_Liquid/testing_code.jl
@@ -0,0 +1,14 @@
+GPfolder=string("/home/moogmt/PHYSIX_Utils/GPlib/Julia/")
+push!(LOAD_PATH, GPfolder)
+
+using atom_mod
+using cell_mod
+using cube_mod
+using clustering
+using cpmd
+
+folder_in=string("/media/moogmt/Elements/CO2/8.82/3000K/1-run/")
+
+file=string(folder_in,"FTRAJECTORY")
+
+cpmd.getNbStepAtomsFTRAJ(file)
