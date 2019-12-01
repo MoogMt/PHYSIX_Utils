50d9b24bdc1bf80b89d62fca0eb92d6850c72422
MoogMt <moog.mathieu@gmail.com>
2019-12-01 21:19:08 +0100

    deleted:    histogram.jl 	deleted:    markov_test2.jl 	deleted:    molecules.jl 	deleted:    plot_scattering.jl 	modified:   statistics.jl

--- GPlib/Julia/statistics.jl
+++ GPlib/Julia/statistics.jl
@@ -48,4 +48,37 @@ function bootStrap( data::Vector{T1}, n_boot::T2) where { T1 <: Real, T2 <: Int 
     return bs1.t0[1], stderror(bs1)[1]
 end
 
+function minMax( data::Vector{T1} ) where { T1 <: Real }
+    min_ = data[1]
+    max_ = data[1]
+    for i=1:size(data)
+        if min_ > data[i]
+            min_=data[i]
+        end
+        if max_ < data[i]
+            max_=data[i]
+        end
+    end
+    return min_,max_
+end
+
+function histogram( data::Vector{T1}, nb_box::T2) where { T1 <: Real , T2 <: Int }
+    min_,max_ = minMax(data)
+    histogram = zeros( Int, size(data)[1] )
+    for i=1:size(data)[1]
+        histogram[ Int(trunc(data[i]/nb_box))+1 ]  += 1
+    end
+    return histogram
+end
+
+function histogramNormed( data::Vector{T1}, nb_box::T2) where { T1 <: Real , T2 <: Int }
+    min_,max_ = minMax(data)
+    histogram_normed = zeros( Real, size(data)[1] )
+    for i=1:size(data)[1]
+        histogram[ Int(trunc(data[i]/nb_box))+1 ]  += 1
+    end
+    histogram /= sum(histogram)
+    return histogram
+end
+
 end
