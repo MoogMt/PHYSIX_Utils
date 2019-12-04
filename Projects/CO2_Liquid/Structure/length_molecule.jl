0ae1280c9a076fefcea7393bffbc506367eeab3f
MoogMt <moog.mathieu@gmail.com>
2019-12-04 12:28:18 +0100

    modified:   Structure/length_molecule.jl

--- Projects/CO2_Liquid/Structure/length_molecule.jl
+++ Projects/CO2_Liquid/Structure/length_molecule.jl
@@ -28,11 +28,6 @@ nb_molecules=size(molecules)[1]
 matrices=graph.extractAllMatrixForTrees( matrix, molecules )
 adjacent_molecule=getAllAdjacentVertex(matrices[1])
 
-start=molecules[1][1]
-
-nb_atoms=size(molecules[1])[1]
-visited=zeros(Int,nb_atoms)
-
 function unWrapOrtho!( positions::Array{T1,2}, origin::T2, target::T3, cell::T4  ) where { T1 <: Real, T2 <: Int, T3 <: Int, T4 <: Cell_param }
     for i=1:3
         dx=positions[origin,i]-positions[target,i]
@@ -46,14 +41,15 @@ function unWrapOrtho!( positions::Array{T1,2}, origin::T2, target::T3, cell::T4 
 end
 
 function recursiveExplorativeUnWrap( visited::Vector{T1}, matrix::Array{T2,2}, adjacency_table::Vector{T3}, positions::Array{T4,2} , cell::T5, target::T6, index_atoms::Vector{T7}, cut_off::T8 ) where { T1 <: Int, T2 <: Real, T3 <: Any, T4 <: Real, T5 <: cell_mod.Cell_param, T6 <: Int, T7 <: Int, T8 <: Real }
+    print(target,"\n")
     visited[target]=1
     nb_neighbor=size(adjacency_table[target])[1]
     for neigh=1:nb_neighbor
         if visited[neigh] == 0
             unWrapOrtho!( positions, index_atoms[target], index_atoms[neigh], cell )
-            test=recursiveExplorativeUnWrap(visited,matrix,adjacency_table,positions,cell,neigh,target,index_atoms)
+            test=recursiveExplorativeUnWrap(visited,matrix,adjacency_table,positions,cell,neigh,index_atoms,cut_off)
             # If infinite molecule is spotted, we stop
-            if ! test
+            if test == -1
                 return -1
             end
         elseif neigh != target
@@ -65,6 +61,13 @@ function recursiveExplorativeUnWrap( visited::Vector{T1}, matrix::Array{T2,2}, a
     end
     return
 end
+
+start=molecules[1][1]
+nb_atoms=size(molecules[1])[1]
+visited=zeros(Int,nb_atoms)
+positions_local=traj[1].positions
+
+test=recursiveExplorativeUnWrap(visited,matrices[1],adjacent_molecule,positions_local,cell,1,molecules[1],cut_off)
 
 for molecule=1:nb_molecules
 end
