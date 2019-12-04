e437fe65df3d814eecdf037ab5a8ae69cf779479
MoogMt <moog.mathieu@gmail.com>
2019-12-04 11:03:15 +0100

    modified:   ../../GPlib/Julia/geom.jl 	modified:   Structure/length_molecule.jl

--- GPlib/Julia/geom.jl
+++ GPlib/Julia/geom.jl
@@ -1,39 +1,13 @@
 module geom
 
-# Calculate the distance between two vectors
+using LinearAlgebra
+
 function distance( vec1::Vector{T1}, vec2::Vector{T2} ) where { T1 <: Real , T2 <: Real }
-  if size(vec1)[1] != size(vec2)[1]
-    error(string("Size mismatch between the two vectors ",size(vec1)[1]," for first argument and",size(vec1)[2],"for second argument.\n"))
-  end
-  distance=0
-  for i=1:size(vec1)[1]
-    distance = distance + (vec1[i]-vec2[i])^2
-  end
-  return sqrt(distance)
-end
-
-function norm( vector::Vector{T1} ) where { T1 <: Real }
-    norm = 0;
-    for i=1:size(vector)[1]
-        norm += vector[i]*vector[i];
-    end
-    return sqrt(norm)
-end
-
-function scalar( vector1::Vector{T1} , vector2::Vector{T2} ) where { T1 <: Real, T2 <: Real }
-    if size(vector1)[1] != size(vector2)[1]
-        # Should return an exception here...
-        return false;
-    end
-    scalar = 0;
-    for i=1:size(vector1)[1]
-        scalar += vector1[i]*vector2[i]
-    end
-    return scalar
+    return LinearAlgebra.norm(vec1-vec2)
 end
 
 function dist2Line( vector_2_line::Vector{T1}, line_vector::Vector{T2} ) where { T1 <: Real, T2 <: Real}
-    return abs(scalar(vector_2_line,line_vector))/norm(line_vector)
+    return abs(dot(vector_2_line,line_vector))/LinearAlgebra.norm(line_vector)
 end
 
 function pointFromLine( point::Vector{T1}, line_vector::Vector{T2} , point_in_line::Vector{T3} ) where { T1 <: Real, T2 <: Real, T3 <: Real }
--- Projects/CO2_Liquid/Structure/length_molecule.jl
+++ Projects/CO2_Liquid/Structure/length_molecule.jl
@@ -10,6 +10,7 @@ using contact_matrix
 using filexyz
 using graph
 using exp_data
+using geom
 
 V=9.1
 T=3000
@@ -33,24 +34,24 @@ start=moleculs[1][1]
 nb_atoms=size(molecules[1])[1]
 visited=zeros(Int,nb_atoms)
 
-function unWrap( positions::Array{T1,2}, origin::T2, target::T3, index_atoms::Vector{T4} ) where { T1 <: Real, T2 <: Int, T3 <: Int, T4 <: Int }
+function unWrap!( positions::Array{T1,2}, origin::T2, target::T3  ) where { T1 <: Real, T2 <: Int, T3 <: Int }
 
 end
 
-function recursiveExplorativeUnWrap( visited::Vector{T1}, matrix::Array{T2,2}, adjacency_table::Vector{T3}, positions::Array{T4,2} , cell::T5, target::T6, index_atoms::Vector{T7} ) where { T1 <: Int, T2 <: Real, T3 <: Any, T4 <: Real, T5 <: cell_mod.Cell_param, T6 <: Int, T7::Int }
+function recursiveExplorativeUnWrap( visited::Vector{T1}, matrix::Array{T2,2}, adjacency_table::Vector{T3}, positions::Array{T4,2} , cell::T5, target::T6, index_atoms::Vector{T7}, cut_off::T8 ) where { T1 <: Int, T2 <: Real, T3 <: Any, T4 <: Real, T5 <: cell_mod.Cell_param, T6 <: Int, T7::Int, T8 <: Real }
     visited[target]=1
     nb_neighbor=size(adjacency_table[target])[1]
     for neigh=1:nb_neighbor
         if visited[neigh] == 0
-            unWrap(positions,target,neigh,index_atoms)
+            unWrap!( positions, index_atoms[target], index_atoms[neigh] )
             test=recursiveExplorativeUnWrap(visited,matrix,adjacency_table,positions,cell,neigh,target,index_atoms)
             # If infinite molecule is spotted, we stop
             if ! test
                 return -1
             end
         else if neigh != target
-            if cell_mod.distance(positions[target,:],positions[neigh,:]) > cut_off
-                # Spotted infinite loop
+            if geom.distance(positions[index_atoms[target],:],positions[index_atoms[neigh],:]) > cut_off
+                # Spotted infinite loop; stops the search
                 return -1
             end
         end
