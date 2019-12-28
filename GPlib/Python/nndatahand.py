745dd36c3f101ea7074f73673577cbf76db7d9d7
MoogMt <moog.mathieu@gmail.com>
2019-12-28 19:03:06 +0100

    modified:   descriptors.py 	modified:   nndatahand.py

--- GPlib/Python/nndatahand.py
+++ GPlib/Python/nndatahand.py
@@ -33,3 +33,21 @@ def choseTrainDataByIndex(metadata,structures,energies,chosen_index):
 def choseTrainDataRandom(metadata,structures,energies):    
     chosen_index = np.random.choice(metadata['total_size_set'],size=int(metadata['train_fraction']*metadata['total_size_set']),replace=metadata['replace'])
     return choseTrainDataByIndex(metadata,structures,energies,chosen_index)
+
+def pcaSelectBestParams(descriptors,nb_features,N_PCA):
+    pca_O = PCA(n_components=N_PCA).fit(descriptors[:,:2,:].reshape(descriptors[:,:2,:].shape[0]*2,nb_features))
+    pca_H = PCA(n_components=N_PCA).fit(descriptors[:,2:,:].reshape(descriptors[:,2:,:].shape[0]*5,nb_features))
+    print("Precision of new features [O,H] = ",[np.cumsum(pca_O.explained_variance_ratio_)[-1],np.cumsum(pca_H.explained_variance_ratio_)[-1]])        
+    return [pca_O, pca_H]
+    
+def scale_descriptors(data,descriptors):
+    is_train = data['is_train']
+    nb_features = descriptors.shape[2]
+    scaler = []
+    scaler.append(StandardScaler())
+    scaler[0].fit(descriptors[is_train][:,0:2,:].reshape(int(is_train.sum()*2),nb_features))    
+    descriptors[:,0:2,:] = scaler[0].transform(descriptors[:,0:2,:].reshape(descriptors[:,0:2,:].shape[0]*2,nb_features)).reshape(descriptors.shape[0],2,nb_features)
+    scaler.append(StandardScaler())
+    scaler[1].fit(descriptors[is_train][:,2:,:].reshape(int(is_train.sum()*5),nb_features))    
+    descriptors[:,2:,:] = scaler[1].transform(descriptors[:,2:,:].reshape(descriptors[:,2:,:].shape[0]*5,nb_features)).reshape(descriptors.shape[0],5,nb_features)
+    return descriptors, scaler
