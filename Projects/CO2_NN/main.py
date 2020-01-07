4bf5b5763c30f2b5acbc9b378cb41b4496b9c137
MoogMt <moog.mathieu@gmail.com>
2020-01-07 15:25:35 +0100

    deleted:    nndatahand.py 	modified:   nnmetadata.py 	modified:   ../../Projects/CO2_NN/main.py

--- Projects/CO2_NN/main.py
+++ Projects/CO2_NN/main.py
@@ -7,7 +7,6 @@ Created on Thu Dec 26 07:50:29 2019
 """
 
 import nnmetadata as mtd
-import nndatahand as dth
 import filexyz as xyz
 import cpmd 
 import descriptors as desc
@@ -63,17 +62,20 @@ metadata=mtd.getNbAtomsPerSpecies(traj,metadata)
 metadata['n_jobs'] = 8 # Number of parallel cores to use (CPU)
 metadata['train_set_size'] = 400
 metadata['total_size_set'] = len(energies)
-metadata,data_train = dth.choseTrainDataRandom(metadata,traj,energies)
+metadata,data_train = mtd.choseTrainDataRandom(metadata,traj,energies)
 # Creating testing set
 metadata["test_set_size"] = 500
+data_test = mtd.ChooseDataExclusion(metadata,traj,energies)
 # Build descriptors from positions (train set only)
 sigma_  = 0.9  # 3*sigma ~ 2.7A relatively large spread
 cutoff_ = 3.5 # cut_off SOAP, 
 nmax_   = 2 
 lmax_   = 2
 metadata, descriptors = desc.createDescriptorsSOAP(data_train,metadata,sigma_SOAP=sigma_,cutoff_SOAP=cutoff_,nmax_SOAP=nmax_,lmax_SOAP=lmax_)
-descriptors, scalers = dth.scaleData(descriptors,metadata)
+descriptors, scalers = mtd.scaleData(descriptors,metadata)
 data_train=data_train.join(pd.DataFrame({'descriptor':list(descriptors)}))
+metadata, descriptors = desc.createDescriptorsSOAP(data_test,metadata,sigma_SOAP=sigma_,cutoff_SOAP=cutoff_,nmax_SOAP=nmax_,lmax_SOAP=lmax_)
+data_test=data_test.join(pd.DataFrame({'descriptor':list(descriptors)}))
 #=============================================================================#
 
 # BUILDING NETWORK
@@ -86,10 +88,6 @@ if metadata["plot_network"]:
 
 # TRAINING NETWORK
 #=============================================================================#
-
-data_test="loutre" # To be done
-metadata, descriptors = desc.createDescriptorsSOAP(data_test,metadata,sigma_SOAP=sigma_,cutoff_SOAP=cutoff_,nmax_SOAP=nmax_,lmax_SOAP=lmax_)
-data_test=data_test.join(pd.DataFrame({'descriptor':list(descriptors)}))
-
+data_train,data_test,mean_error,metadata = behler.train(data_train,data_test,model,metadata)
 #=============================================================================#
 
