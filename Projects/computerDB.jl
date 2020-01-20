4b2db7c388ed963cad5d3182cb9dcc8bfa955a4f
MoogMt <moog.mathieu@gmail.com>
2020-01-20 11:46:47 +0100

    new file:   Projects/computerDB.jl

---
+++ Projects/computerDB.jl
@@ -0,0 +1,15 @@
+module computerDB
+
+computers_names = [
+"hp-physix5",
+"joliotite",
+"OtterCentral"
+]
+
+computers_pathsCO2=[
+"/media/moogmt/Stock/Mathieu/CO2/AIMD/Liquid/PBE-MT/",
+"/media/mathieu/Elements/CO2/",
+"/media/moogmt/Element/CO2/"
+]
+
+end
