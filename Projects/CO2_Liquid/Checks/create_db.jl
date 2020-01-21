6d2704fafa5c645f47cab09fb6e52ce36ca9b54d
MoogMt <moog.mathieu@gmail.com>
2020-01-21 11:13:09 +0100

    modified:   ../CO2_Liquid/Checks/create_db.jl 	new file:   energyPredict.py

--- Projects/CO2_Liquid/Checks/create_db.jl
+++ Projects/CO2_Liquid/Checks/create_db.jl
@@ -32,33 +32,33 @@ standard_stride   = 5
 Volumes=[10.0,9.8,9.5,9.4,9.375,9.35,9.325,9.3,9.25,9.2,9.15,9.1,9.05,9.0,8.82]
 Temperatures=[ 2000, 2500, 3000 ]
 
-V=8.82
-T=3000
-run=1
-folder_target=string(folder_base,V,"/",T,"K/",run,"-run/")
-file_stress_out=string(folder_target, "STRESS_db")
-file_pressure_out=string(folder_target, "Pressure_db.dat")
-file_traj_out = string(folder_target,"TRAJEC_db.xyz")
-file_ftraj_out = string(folder_target,"FTRAJECTORY_db")
-file_energy_out = string(folder_target,"ENERGIES_db")
 
 target_timestep=conversion.hatime2fs*standard_timestep*standard_stride
 
-cpmd.buildingDataBase( folder_target, file_stress_out, file_pressure_out, file_traj_out, file_ftraj_out, file_energy_out, target_timestep )
-
 # Target Timestep
-
 for V in Volumes
     for T in Temperatures
-        n_run = 1
         check = true
+        run=1
         while check
-            folder_target=string(folder_base,V,"/",T,"K/")
-            if ! isfile( isdir(folder_target) )
+            folder_target=string(folder_base,V,"/",T,"K/",run,"-run/")
+            if ! isdir(folder_target)
                 check = false
-            end
-            if ! buildingDB( folder_target, timestep_target )
-                print("Problem at "V," ",T,"K ",n_run," !\n")
+                continue
+            else
+                file_stress_out = string(folder_target, "STRESS_db")
+                file_pressure_out = string(folder_target, "Pressure_db.dat")
+                file_traj_out = string(folder_target,"TRAJEC_db.xyz")
+                file_ftraj_out = string(folder_target,"FTRAJECTORY_db")
+                file_energy_out = string(folder_target,"ENERGIES_db")
+                test=cpmd.buildingDataBase( folder_target, file_stress_out, file_pressure_out, file_traj_out, file_ftraj_out, file_energy_out, target_timestep )
+                # buildingDataBase contains all necessary error messages, at least in theory...
+                if test
+                    print("OK for V=",V," T=",T,"K run=",run," \n")
+                else
+                    print("Problem for V=",V," T=",T,"K run=",run," \n")
+                end
+                run += 1
             end
         end
     end
