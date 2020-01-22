6975f6b00de6432989b8a4e68c2147a09e75bcef
MoogMt <moog.mathieu@gmail.com>
2020-01-22 14:52:38 +0100

    modified:   Projects/CO2_Liquid/Checks/create_db_fix.jl

--- Projects/CO2_Liquid/Checks/create_db_fix.jl
+++ Projects/CO2_Liquid/Checks/create_db_fix.jl
@@ -30,78 +30,108 @@ standard_timestep = 40
 standard_stride   = 5
 
 # T,V
-V = 8.8
+V = 10.0
 T = 3000
-run_ = 1
+run_ = 2
 
 timestep_target=conversion.hatime2fs*standard_timestep*standard_stride
 
 folder_target=string(folder_base,V,"/",T,"K/",run_,"-run/")
 
+#--------------------------------------------------------------------------------
 file_stress_out = string(folder_target, "STRESS_db")
 file_pressure_out = string(folder_target, "Pressure_db.dat")
 file_traj_out = string(folder_target,"TRAJEC_db.xyz")
 file_ftraj_out = string(folder_target,"FTRAJECTORY_db")
 file_energy_out = string(folder_target,"ENERGIES_db")
+#--------------------------------------------------------------------------------
 
 # Input parameters
+#--------------------------------------------------------------------------------
 file_input=string(folder_target,"input")
 stride_stress = cpmd.readIntputStrideStress( file_input )
 stride_traj   = cpmd.readIntputStrideTraj( file_input )
 timestep_sim  =  cpmd.readInputTimestep( file_input )
+#--------------------------------------------------------------------------------
 
 # Fixing strides computation
+#--------------------------------------------------------------------------------
 n_stress = round( Int, timestep_target/( timestep_sim*stride_stress ) )
 n_traj   = round( Int, timestep_target/( timestep_sim*stride_traj ) )
 n_energy = round( Int, timestep_target/( timestep_sim ) )
 n_ftraj  = round( Int, timestep_target/( timestep_sim*stride_traj ) )
+#--------------------------------------------------------------------------------
 
+#--------------------------------------------------------------------------------
 file_energy_in = string(folder_target,"ENERGIES")
 file_trajec_in = string(folder_target,"TRAJEC.xyz")
 file_stress_in = string(folder_target,"STRESS")
 file_ftrajectory_in = string(folder_target,"FTRAJECTORY")
+#--------------------------------------------------------------------------------
 
+#--------------------------------------------------------------------------------
 nb_step_stress = getNbStepStress( file_stress_in )
-
-nb_step_stress = utils.nbStepStriding( nb_step_stress, n_stress )
+if nb_step_stress != false
+    nb_step_stress = utils.nbStepStriding( nb_step_stress, n_stress )
+end
 #-------------------------------------------
 nb_step_ftraj, nb_atoms = getNbStepAtomsFtraj( file_ftrajectory_in )
-nb_step_ftraj  = utils.nbStepStriding( nb_step_ftraj, n_ftraj )
+if nb_step_ftraj != false
+    nb_step_ftraj  = utils.nbStepStriding( nb_step_ftraj, n_ftraj )
+end
 #-------------------------------------------
 nb_step_energy = getNbStepEnergies( file_energy_in )
-nb_step_energy = utils.nbStepStriding( nb_step_energy, n_energy )
+if nb_step_energy != false
+    nb_step_energy = utils.nbStepStriding( nb_step_energy, n_energy )
+end
 #-------------------------------------------
 nb_step_traj = filexyz.getNbSteps( file_trajec_in )
-nb_step_traj = utils.nbStepStriding( nb_step_traj, n_traj )
-#-------------------------------------------
+if nb_step_traj != false
+    nb_step_traj = utils.nbStepStriding( nb_step_traj, n_traj )
+end
+#--------------------------------------------------------------------------------
 
 #-------------------------------------------
 print("traj_step: ",nb_step_traj,"\n")
 print("ftraj_step: ",nb_step_ftraj,"\n")
 print("energy_step: ",nb_step_energy,"\n")
 print("stress_step: ",nb_step_stress,"\n")
-target_length = min( nb_step_traj,  nb_step_ftraj, nb_step_energy, nb_step_stress ) #,
+#-------------------------------------------
+
+#-------------------------------------------
+target_length = min( nb_step_traj, nb_step_ftraj, nb_step_energy, nb_step_stress ) #
+if target_length == 0
+    print("Error: target_length = 0.\n")
+end
 #-------------------------------------------
 
 nb_ignored=0
 #---------------------------------------------
-stress_tensor = cpmd.readStress( file_stress_in, n_stress, nb_ignored, target_length )
-cpmd.writeStress( file_stress_out, stress_tensor )
-utils.writeData( file_pressure_out, press_stress.computePressure(stress_tensor) )
-stress_tensor=[] # Clearing memory
+if nb_step_stress != false
+    stress_tensor = cpmd.readStress( file_stress_in, n_stress, nb_ignored, target_length )
+    cpmd.writeStress( file_stress_out, stress_tensor )
+    utils.writeData( file_pressure_out, press_stress.computePressure(stress_tensor) )
+    stress_tensor=[] # Clearing memory
+end
 #---------------------------------------------
-filexyz.writeXYZ( file_traj_out, filexyz.readFileAtomList( file_trajec_in, n_traj, nb_ignored, target_length ) )
+if nb_step_traj != false
+    filexyz.writeXYZ( file_traj_out, filexyz.readFileAtomList( file_trajec_in, n_traj, nb_ignored, target_length ) )
+end
 #---------------------------------------------
-positions,velocities,forces=cpmd.readFtraj( file_ftrajectory_in, n_ftraj, nb_ignored, target_length )
-cpmd.writeFtraj( file_ftraj_out, positions, velocities, forces )
-positions=[]
-velocities=[]
-forces=[]
+if nb_step_ftraj != false
+    positions,velocities,forces=cpmd.readFtraj( file_ftrajectory_in, n_ftraj, nb_ignored, target_length )
+    cpmd.writeFtraj( file_ftraj_out, positions, velocities, forces )
+    positions=[]
+    velocities=[]
+    forces=[]
+end
 #---------------------------------------------
-temp, epot, etot, msd, comp = cpmd.readEnergies( file_energy_in, n_energy, nb_ignored, target_length )
-cpmd.writeEnergies( file_energy_out, temp, epot, etot, msd, comp )
-epot=[]
-etot=[]
-msd=[]
-comp=[]
+if nb_step_energy != false
+    temp, epot, etot, msd, comp = cpmd.readEnergies( file_energy_in, n_energy, nb_ignored, target_length )
+    cpmd.writeEnergies( file_energy_out, temp, epot, etot, msd, comp )
+    epot=[]
+    etot=[]
+    msd=[]
+    comp=[]
+end
 #---------------------------------------------
