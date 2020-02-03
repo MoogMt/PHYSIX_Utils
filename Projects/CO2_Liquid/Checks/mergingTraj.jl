1f16e83bc02ffd6974c926e2d544732fd10fb1ac
MoogMt <moog.mathieu@gmail.com>
2020-02-03 23:36:54 +0100

    modified:   mergingTraj.jl

--- Projects/CO2_Liquid/Checks/mergingTraj.jl
+++ Projects/CO2_Liquid/Checks/mergingTraj.jl
@@ -28,8 +28,11 @@ cut_off_rmsd=0.2
 
 for V in Volumes
     for T in Temperatures
-        print("Rolling check on ",V," ",T,"K\n")
+        print("Merging trajectories in ",V," ",T,"K\n")
         folder_local=string(folder_base,V,"/",T,"K/")
+
+        total_nb_step=0
+        total_time=[]
 
         for nbrun in runs
 
@@ -40,53 +43,38 @@ for V in Volumes
             if ! isfile(file_input)
                 break
             end
-            time_step=cpmd.readInputTimestep( file_input )
-            stride_stress=cpmd.readIntputStrideStress( file_input )
-            stride_traj=cpmd.readIntputStrideTraj(file_input)
 
-            file_stress=string(folder_in,"STRESS")
-            stress,test=cpmd.readStress(file_stress)
-            if ! test
-                break
+            if ! isfile( string(folder_in,"FLAG") ) || ! isfile( string(folder_in,"FLAG2") ) || ! isfile( string(folder_in,"FLAG") )
+                print("FLAG spotted in ",V," ",T,"K",)
             end
-            pressure=press_stress.computePressure(stress)
-            size_pressure=size(pressure)[1]
-
-            file_energy=string(folder_in,"ENERGIES")
-            temperature, e_pot,e_tot,msd,comp_time,test=cpmd.readEnergyFile( file_energy )
-            if ! test
-                break
-            end
-            size_energy=size(temperature)[1]
-
-            file_traj=string(folder_in,"TRAJEC.xyz")
-            traj,test=filexyz.readFastFile(file_traj)
-            if ! test
-                break
-            end
-            size_traj=size(traj)[1]
 
             if nbrun > 1
-                folder_in_2=string(folder_base,V,"/",T,"K/",nbrun-1,"-run/")
-                file_traj_2=string(folder_in_2,"TRAJEC.xyz")
-                traj2,test=filexyz.readFastFile(file_traj_2)
-                size_traj2=size(traj2)[1]
-                connexion_step=-1
-                for step=size_traj2:-1:1
-                    if exp_data.computeRMSD(traj[1],traj2[step]) < cut_off_rmsd
-                        connexion_step=step
-                        break
-                    end
-                end
-                if size_traj2 - connexion_step > stride_traj
-                    print("CHECK FOR CUT, V=",V," T=",T,"K runs:",nbrun-1,"-",nbrun,"\n")
-                    print("connexion step: ",connexion_step,"\n")
-                    for step=size_traj2:-1:size_traj2-stride_traj
-                        print("step: ",step," RMSD: ",exp_data.computeRMSD(traj[1],traj2[Int(step)]),"\n")
-                    end
-                end
-            end
 
+                traj_current_curr = filexyz.readFileAtomList( folder_local, nbrun, "-run/TRAJEC_db.xyz")
+                traj_current_prev = filexyz.readFileAtomList( folder_local, nbrun-1, "-run/TRAJEC_db.xyz")
+
+                if exp_data.computeRMSD(traj[1],traj2[Int(step)]) < cut_off_rmsd
+                    print("Faillure to merge simply at ",nbrun,"\n")
+                    handle_out = open( string(folder_local,nbrun,"-run/FLAG4"), "w")
+                    write(handle_out,string("CHECK"))
+                    close(handle_out)
+                    merge = false
+                    break
+                else
+                    times = atom_mod.getNbStep( folder_local, nbrun, "-run/TRAJEC_db.xyz" )
+                    nb_total += times
+                    push!( total_time, times )
+                end
+            else
+                times = atom_mod.getNbStep( folder_local, nbrun, "-run/TRAJEC_db.xyz" )
+                nb_total += times
+                push!( total_time, times )
+            end
         end
+
+        if nb_total < target_length && merge
+            print("Total length of simulation is not ")
+        end
+
     end
 end
