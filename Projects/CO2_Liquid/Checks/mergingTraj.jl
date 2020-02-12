f9d83352dc94bbb8ff62f1b9931655f6cd416941
Mathieu Moog <moog.mathieu@gmail.com>
2020-02-12 14:19:37 +0100

    modified:   mergingTraj.jl 	deleted:    ../check_all.jl 	deleted:    ../plot_scaterring2.jl

--- Projects/CO2_Liquid/Checks/mergingTraj.jl
+++ Projects/CO2_Liquid/Checks/mergingTraj.jl
@@ -21,8 +21,8 @@ folder_base="/media/moogmt/Elements/CO2/"
 folder_base="/media/mathieu/Elements/CO2/"
 
 # T,V
-Volumes=[10.0,9.8,9.5,9.4,9.375,9.35,9.325,9.3,9.25,9.2,9.15,9.1,9.05,9.0,8.82]
-Temperatures=[2000,2500,3000]
+Volumes=[10.0,9.8,9.5,9.4,9.375,9.35,9.325,9.3,9.25,9.2,9.15,9.1,9.05,9.0,8.82,8.8,8.6]
+Temperatures=[1750]
 runs=[1,2,3,4]
 
 cut_off_rmsd=0.2
@@ -60,6 +60,7 @@ for V in Volumes
                 if isfile( string(folder_in,"FLAG3") )
                     print("FLAG3\n")
                 end
+                merge = false
             end
             if nbrun > 1
 
@@ -105,56 +106,65 @@ for V in Volumes
             # Shorting TRAJEC.xyz
             #---------------------------------------------
             check=true
-            traj = filexyz.readFileAtomList( string( folder_local, 1, "-run/TRAJEC_db.xyz" ) )
-            filexyz.writeXYZ( string( folder_local, "TRAJEC_fdb.xyz" ), traj[total_time[1]+1-target_length:total_time[1] ] )
-            cell = cell_mod.Cell_param(V,V,V)
-            filexyz.writeXYZ( string( folder_local, "TRAJEC_fdb_wrapped.xyz" ), cell_mod.wrap( traj[total_time[1]+1-target_length:total_time[1] ], cell ) )
-            nb_atoms = size(traj[1].names)[1]
-            # Clean up
-            traj=0
-            # Shorting FTRAJECTORY
-            #---------------------------------------------
-            positions, velocities, forces = cpmd.readFtraj( string( folder_local, 1, "-run/FTRAJECTORY_db" )  )
-            positions_final = positions[total_time[1]+1-target_length:total_time[1],:,:]
-            velocities_final = velocities[total_time[1]+1-target_length:total_time[1],:,:]
-            forces_final = forces[total_time[1]+1-target_length:total_time[1],:,:]
-            cpmd.writeFtraj( string( folder_local, "FTRAJECTORY_fdb" ), positions_final, velocities_final, forces_final )
-            # Clean up
-            positions=0
-            velocities=0
-            forces=0
-            positions_final=0
-            velocities_final=0
-            forces_final = 0
-            # Shorting ENERGIES
-            #---------------------------------------------
-            temp,epot,etot,msd,comp = cpmd.readEnergies( string( folder_local, 1, "-run/ENERGIES_db" )  )
-            temp_final = temp[total_time[1]+1-target_length:total_time[1] ]
-            epot_final = epot[total_time[1]+1-target_length:total_time[1] ]
-            etot_final = etot[total_time[1]+1-target_length:total_time[1] ]
-            msd_final = msd[total_time[1]+1-target_length:total_time[1] ]
-            comp_final = comp[total_time[1]+1-target_length:total_time[1] ]
-            cpmd.writeEnergies( string( folder_local, "ENERGIES_fdb" ), temp_final, epot_final, etot_final, msd_final, comp_final )
-            temp = 0
-            epot = 0
-            etot = 0
-            msd = 0
-            comp = 0
-            temp_final = 0
-            epot_final = 0
-            etot_final = 0
-            msd_final = 0
-            comp_time = 0
-            # Shorting STRESS
-            #---------------------------------------------
-            stress_tensor = cpmd.readStress( string( folder_local, 1, "-run/STRESS_db" )   )
-            stress_tensor_final = stress_tensor[total_time[1]+1-target_length:total_time[1],:,:]
-            stress_tensor=0
-            cpmd.writeStress( string( folder_local, "STRESS_fdb" ), stress_tensor_final )
-            pressure_final = press_stress.computePressure( stress_tensor_final )
-            stress_tensor_final = 0
-            press_stress.writePressure( string( folder_local, "Pressure_fdb" ), pressure_final )
-            pressure_final = 0
+            if total_time[1]+1-target_length < 1
+                print("Only one trajectory and it is to short to get to the target length.\n")
+                print("Target length: ",target_length,"\n")
+                print("Size trajectory: ",total_time[1]+1,"\n")
+                check=false
+                continue
+            else
+                traj = filexyz.readFileAtomList( string( folder_local, 1, "-run/TRAJEC_db.xyz" ) )
+                traj_final = traj[total_time[1]+1-target_length:total_time[1] ]
+                filexyz.writeXYZ( string( folder_local, "TRAJEC_fdb.xyz" ), traj_final )
+                cell = cell_mod.Cell_param(V,V,V)
+                filexyz.writeXYZ( string( folder_local, "TRAJEC_fdb_wrapped.xyz" ), cell_mod.wrap( traj_final,cell ) )
+                nb_atoms = size(traj[1].names)[1]
+                # Clean up
+                traj=0
+                # Shorting FTRAJECTORY
+                #---------------------------------------------
+                positions, velocities, forces = cpmd.readFtraj( string( folder_local, 1, "-run/FTRAJECTORY_db" )  )
+                positions_final = positions[total_time[1]+1-target_length:total_time[1],:,:]
+                velocities_final = velocities[total_time[1]+1-target_length:total_time[1],:,:]
+                forces_final = forces[total_time[1]+1-target_length:total_time[1],:,:]
+                cpmd.writeFtraj( string( folder_local, "FTRAJECTORY_fdb" ), positions_final, velocities_final, forces_final )
+                # Clean up
+                positions=0
+                velocities=0
+                forces=0
+                positions_final=0
+                velocities_final=0
+                forces_final = 0
+                # Shorting ENERGIES
+                #---------------------------------------------
+                temp,epot,etot,msd,comp = cpmd.readEnergies( string( folder_local, 1, "-run/ENERGIES_db" )  )
+                temp_final = temp[total_time[1]+1-target_length:total_time[1] ]
+                epot_final = epot[total_time[1]+1-target_length:total_time[1] ]
+                etot_final = etot[total_time[1]+1-target_length:total_time[1] ]
+                msd_final = msd[total_time[1]+1-target_length:total_time[1] ]
+                comp_final = comp[total_time[1]+1-target_length:total_time[1] ]
+                cpmd.writeEnergies( string( folder_local, "ENERGIES_fdb" ), temp_final, epot_final, etot_final, msd_final, comp_final )
+                temp = 0
+                epot = 0
+                etot = 0
+                msd = 0
+                comp = 0
+                temp_final = 0
+                epot_final = 0
+                etot_final = 0
+                msd_final = 0
+                comp_time = 0
+                # Shorting STRESS
+                #---------------------------------------------
+                stress_tensor = cpmd.readStress( string( folder_local, 1, "-run/STRESS_db" )   )
+                stress_tensor_final = stress_tensor[total_time[1]+1-target_length:total_time[1],:,:]
+                stress_tensor=0
+                cpmd.writeStress( string( folder_local, "STRESS_fdb" ), stress_tensor_final )
+                pressure_final = press_stress.computePressure( stress_tensor_final )
+                stress_tensor_final = 0
+                press_stress.writePressure( string( folder_local, "Pressure_fdb" ), pressure_final )
+                pressure_final = 0
+            end
         else
             if total_nb_step < target_length && ! merge
                 if ! merge
@@ -250,11 +260,11 @@ for V in Volumes
                     end
                     if i == max_nb_run
                         if target_length-total_time[nb_times] > 1
-                            temp_final[ target_length-total_time[nb_times]:target_length ] = temp[1:total_time[i]]
-                            epot_final[ target_length-total_time[nb_times]:target_length ] = epot[1:total_time[i]]
-                            etot_final[ target_length-total_time[nb_times]:target_length ] = etot[1:total_time[i]]
-                            msd_final[ target_length-total_time[nb_times]:target_length ] = msd[1:total_time[i]]
-                            comp_final[ target_length-total_time[nb_times]:target_length ] = comp[1:total_time[i]]
+                            temp_final[ target_length+1-total_time[nb_times]:target_length ] = temp[1:total_time[i]]
+                            epot_final[ target_length+1-total_time[nb_times]:target_length ] = epot[1:total_time[i]]
+                            etot_final[ target_length+1-total_time[nb_times]:target_length ] = etot[1:total_time[i]]
+                            msd_final[ target_length+1-total_time[nb_times]:target_length ] = msd[1:total_time[i]]
+                            comp_final[ target_length+1-total_time[nb_times]:target_length ] = comp[1:total_time[i]]
                         else
                             temp_final[1:target_length] = temp[total_time[i]+1-target_length:total_time[i] ]
                             epot_final[1:target_length] = epot[total_time[i]+1-target_length:total_time[i] ]
@@ -265,17 +275,17 @@ for V in Volumes
                             break
                         end
                     elseif target_length-sum(total_time[i:nb_times]) > 1
-                        temp_final[target_length-sum(total_time[i:nb_times]):target_length-sum(total_time[i+1:nb_times])] = temp[1:total_time[i]]
-                        epot_final[target_length-sum(total_time[i:nb_times]):target_length-sum(total_time[i+1:nb_times])] = epot[1:total_time[i]]
-                        etot_final[target_length-sum(total_time[i:nb_times]):target_length-sum(total_time[i+1:nb_times])] = etot[1:total_time[i]]
-                        msd_final[target_length-sum(total_time[i:nb_times]):target_length-sum(total_time[i+1:nb_times])] = msd[1:total_time[i]]
-                        comp_final[target_length-sum(total_time[i:nb_times]):target_length-sum(total_time[i+1:nb_times])] = comp[1:total_time[i]]
+                        temp_final[target_length+1-sum(total_time[i:nb_times]):target_length-sum(total_time[i+1:nb_times])] = temp[1:total_time[i]]
+                        epot_final[target_length+1-sum(total_time[i:nb_times]):target_length-sum(total_time[i+1:nb_times])] = epot[1:total_time[i]]
+                        etot_final[target_length+1-sum(total_time[i:nb_times]):target_length-sum(total_time[i+1:nb_times])] = etot[1:total_time[i]]
+                        msd_final[target_length+1-sum(total_time[i:nb_times]):target_length-sum(total_time[i+1:nb_times])] = msd[1:total_time[i]]
+                        comp_final[target_length+1-sum(total_time[i:nb_times]):target_length-sum(total_time[i+1:nb_times])] = comp[1:total_time[i]]
                     else
-                        temp_final[1:target_length-sum(total_time[i+1:nb_times])] = temp[total_time[i]-(target_length-sum(total_time[i+1:nb_times])):total_time[i]]
-                        epot_final[1:target_length-sum(total_time[i+1:nb_times])] = epot[total_time[i]-(target_length-sum(total_time[i+1:nb_times])):total_time[i]]
-                        etot_final[1:target_length-sum(total_time[i+1:nb_times])] = etot[total_time[i]-(target_length-sum(total_time[i+1:nb_times])):total_time[i]]
-                        msd_final[1:target_length-sum(total_time[i+1:nb_times])] = msd[total_time[i]-(target_length-sum(total_time[i+1:nb_times])):total_time[i]]
-                        comp_final[1:target_length-sum(total_time[i+1:nb_times])] = comp[total_time[i]-(target_length-sum(total_time[i+1:nb_times])):total_time[i]]
+                        temp_final[1:target_length-sum(total_time[i+1:nb_times])] = temp[total_time[i]+1-(target_length-sum(total_time[i+1:nb_times])):total_time[i]]
+                        epot_final[1:target_length-sum(total_time[i+1:nb_times])] = epot[total_time[i]+1-(target_length-sum(total_time[i+1:nb_times])):total_time[i]]
+                        etot_final[1:target_length-sum(total_time[i+1:nb_times])] = etot[total_time[i]+1-(target_length-sum(total_time[i+1:nb_times])):total_time[i]]
+                        msd_final[1:target_length-sum(total_time[i+1:nb_times])] = msd[total_time[i]+1-(target_length-sum(total_time[i+1:nb_times])):total_time[i]]
+                        comp_final[1:target_length-sum(total_time[i+1:nb_times])] = comp[total_time[i]+1-(target_length-sum(total_time[i+1:nb_times])):total_time[i]]
                         break
                     end
                 end
@@ -297,16 +307,16 @@ for V in Volumes
                     end
                     if i == max_nb_run
                         if target_length-total_time[nb_times] > 1
-                            stress_tensor_final[target_length-total_time[nb_times]:target_length,:,:] = stress_tensor[1:total_time[i],:,:]
+                            stress_tensor_final[target_length+1-total_time[nb_times]:target_length,:,:] = stress_tensor[1:total_time[i],:,:]
                         else
                             stress_tensor_final[1:target_length,:,:] = stress_tensor[total_time[i]+1-target_length:total_time[i],:,:]
                             check=false
                             break
                         end
                     elseif target_length-sum(total_time[i:nb_times]) > 1
-                        stress_tensor_final[target_length-sum(total_time[i:nb_times]):target_length-sum(total_time[i+1:nb_times]),:,:] = stress_tensor[1:total_time[i],:,:]
+                        stress_tensor_final[target_length+1-sum(total_time[i:nb_times]):target_length-sum(total_time[i+1:nb_times]),:,:] = stress_tensor[1:total_time[i],:,:]
                     else
-                        stress_tensor_final[1:target_length-sum(total_time[i+1:nb_times]),:,:] = stress_tensor[total_time[i]-(target_length-sum(total_time[i+1:nb_times])):total_time[i],:,:]
+                        stress_tensor_final[1:target_length-sum(total_time[i+1:nb_times]),:,:] = stress_tensor[total_time[i]+1-(target_length-sum(total_time[i+1:nb_times])):total_time[i],:,:]
                         break
                     end
                 end
