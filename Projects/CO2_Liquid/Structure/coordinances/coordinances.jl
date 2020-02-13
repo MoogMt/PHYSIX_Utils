de593b9d2a537224bc633dacfd0af75354db9702
Mathieu Moog <moog.mathieu@gmail.com>
2020-02-13 18:34:42 +0100

    modified:   Projects/CO2_Liquid/Structure/coordinances/coordinances.jl

--- Projects/CO2_Liquid/Structure/coordinances/coordinances.jl
+++ Projects/CO2_Liquid/Structure/coordinances/coordinances.jl
@@ -104,16 +104,66 @@ function countRawCoordinances( folder_target::T1, prefix_out_count::T2, prefix_o
 
     return true
 end
+function readCount( file_target::T1 ) where { T1 <: AbstractString }
+    if ! isfile( file_target )
+        return false
+    end
+    handle_in = open( file_target)
+    # Count the number of step and max neighbor
+    nb_step = 0
+    nb_max = 0
+    while ! eof( handle_in )
+        test=readline(handle_in)
+        if nb_step == 0
+            nb_max = size( split( test ) )[1]-1
+        end
+        nb_step+=1
+    end
+    # Read
+    seekstart(handle_in) # back at the begining
+    counts_raw = zeros(nb_step,nb_max)
+    for step=1:nb_step
+        keyword = split( readline( handle_in) )
+        for i=1:nb_max
+            counts_raw[step,i] = parse(Int, keyword[i+1] )
+        end
+    end
+    close(handle_in)
+    return counts_raw
+end
+function readPressure( file_pressure::T1 ) where { T1 <: AbstractString }
+    if ! isfile( file_pressure )
+        return false, false, false
+    end
+    handle_press_in = open( file_pressure )
+    nb_p = 0
+    while !eof( handle_press_in )
+        readline( handle_press_in )
+        nb_p += 1
+    end
+    seekstart( handle_press_in )
+    pressure = zeros(Real, nb_p )
+    delta_p  = zeros(Real, nb_p )
+    volumes  = zeros(Real, nb_p )
+    for i=1:nb_p
+        keyword=split(readline( handle_press_in ))
+        pressure[i] = parse( Float64, keyword[3] )
+        delta_p[i]  = parse( Float64, keyword[4] )
+        volumes[i]  = parse( Float64, keyword[1] )
+    end
+    close( handle_press_in )
+    return pressure, delta_p, volumes
+end
 
 
 cut_off = 1.75
 max_nb_neighbor = 6
 species=["C","O"]
+species_nb=[32,64]
 prefix_count_raw = string("counts_coord_raw")
 prefix_frac_raw = string("frac_coord_raw")
+folder_base=string("/media/mathieu/Elements/CO2/")
 
-# Volumes=[10.0,9.8,9.5,9.4,9.375,9.35,9.325,9.3,9.25,9.2,9.15,9.1,9.05,9.0,8.82,8.8,8.6]
-# Temperatures=[3000,2500,2000]
 
 Volumes=[10.0,9.8,9.5,9.4,9.375,9.35,9.325,9.3,9.25,9.2,9.15,9.1,9.05,9.0,8.82,8.8,8.6]
 Temperatures=[3000,2500,2000]
@@ -122,16 +172,66 @@ Temperatures=[3000,2500,2000]
 restart_count = false
 for T in Temperatures
     for V in Volumes
-        folder_base=string("/media/mathieu/Elements/CO2/")
         folder_target=string(folder_base, V, "/",T,"K/")
-        if ! isfile( string( folder_target, "Data/Coordinance/", prefix_count_raw, "_", species[1], "_",cut_off,".dat" ) ) || restart
+        if ! isfile( string( folder_target, "Data/Coordinance/", prefix_count_raw, "_", species[1], "_",cut_off,".dat" ) ) || restart_count
             countRawCoordinances( folder_target, prefix_count_raw, prefix_frac_raw, max_nb_neighbor, cut_off )
         end
     end
 end
 
+
 # Computing statistics
+n_spec = size(species)[1]
+handle_out_temp=Array{IO}( undef, n_spec, 2 )
+folder_out_general = string( folder_base, "Data/Coordinance/" )
+folder_in_pressure = string( folder_base, "Data/Pressure/" )
+if ! isdir( folder_out_general )
+    Base.Filesystem.mkdir( folder_out_general )
+end
 for T in Temperatures
-    for V in volumes
+    for i_spec=1:n_spec
+        handle_out_temp[i_spec,1] = open( string( folder_out_general, T, "K_coord_", species[i_spec], "stat_glob.dat" ), "w" )
+        handle_out_temp[i_spec,2] = open( string( folder_out_general, T, "K_coord_", species[i_spec], "stat_div.dat" ), "w" )
+    end
+    pressure, delta_press, volumes = readPressure( string(folder_in_pressure,T,"_P_global.dat"))
+    for V in Volumes
+        folder_target=string(folder_base, V, "/",T,"K/")
+        for i_spec=1:n_spec
+            folder_out_loc = string(folder_target, "Data/Coordinance/")
+            target_file = string( folder_out_loc, prefix_count_raw, "_", species[i_spec], "_",cut_off,".dat" )
+            if ! isfile( target_file )
+                continue
+            end
+            press = pressure[ volumes.== V ][1]
+            dp = delta_press[ volumes.== V ][1]
+            counts_ = readCount( target_file )
+            max_nb = size(counts_)[2]
+            nb_step = size(counts_)[1]
+            coord_global=copy(counts_)
+            coord_div=copy(counts_)
+            for step=1:nb_step
+                for i=1:max_nb
+                    coord_global[step,i] = counts_[step,i]*(i-1)/species_nb[i_spec]
+                    coord_div[step,i] = counts_[step,i]/species_nb[i_spec]
+                end
+            end
+            handle_out_loc = open( string(folder_out_loc,"stat_global_coord_",species[i_spec],"_",cut_off,".dat"), "w" )
+            Base.write( handle_out_temp[i_spec],  string( Statistics.mean(coord_global)," ",Statistics.std(coord_global),"\n" ) )
+            Base.write( handle_out_loc, string( Statistics.mean(coord_global)," ",Statistics.std(coord_global) ) )
+            close( handle_out_loc )
+            handle_out_loc_div = open( string(folder_out_loc,"stat_div_coord_",species[i_spec],"_",cut_off,".dat"), "w" )
+            Base.write( handle_out_temp[i_spec,2], string( press, " ", dp, " " ))
+            for i=1:max_nb
+                Base.write( handle_out_temp[i_spec,2],  string( Statistics.mean(coord_div[:,i])," ",Statistics.std(coord_div[:,i])," " ) )
+                Base.write( handle_out_loc_div, string( Statistics.mean(coord_div[:,i])," ",Statistics.std(coord_div[:,i])," " ) )
+            end
+            Base.write( handle_out_temp[i_spec,2], string("\n" ) )
+            Base.write( handle_out_loc_div, string( "\n" ) )
+            close( handle_out_loc_div )
+        end
+    end
+    for i_spec=1:size(species)[1]
+        close( handle_out_temp[i_spec,1] )
+        close( handle_out_temp[i_spec,2] )
     end
 end
