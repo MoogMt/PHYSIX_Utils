86f4fce683aa6d910c02a2f5be8a5052f18c5469
Mathieu Moog <moog.mathieu@gmail.com>
2020-02-15 18:06:50 +0100

    modified:   Projects/CO2_Liquid/Structure/molecules/molecule_analysis.jl

--- Projects/CO2_Liquid/Structure/molecules/molecule_analysis.jl
+++ Projects/CO2_Liquid/Structure/molecules/molecule_analysis.jl
@@ -43,114 +43,137 @@ function writeSizeHistogramGlobal( file_out::T1, hist_time::Array{T2,2} )  where
     end
     close( handle_out )
 end
+function readPressure( file_pressure::T1 ) where { T1 <: AbstractString }
+    if ! isfile( file_pressure )
+        return false, false, false
+    end
+    handle_press_in = open( file_pressure )
+    nb_p = 0
+    while !eof( handle_press_in )
+        readline( handle_press_in )
+        nb_p += 1
+    end
+    seekstart( handle_press_in )
+    pressure = zeros(Real, nb_p )
+    delta_p  = zeros(Real, nb_p )
+    volumes  = zeros(Real, nb_p )
+    for i=1:nb_p
+        keyword=split(readline( handle_press_in ))
+        pressure[i] = parse( Float64, keyword[3] )
+        delta_p[i]  = parse( Float64, keyword[4] )
+        volumes[i]  = parse( Float64, keyword[1] )
+    end
+    close( handle_press_in )
+    return pressure, delta_p, volumes
+end
+
 
 # Thermodynamical values
 Volumes=[10.0,9.8,9.5,9.4,9.375,9.35,9.325,9.3,9.25,9.2,9.15,9.1,9.05,9.0,8.82,8.8,8.6]
-Temperature=[1750,2000,2500,3000,3500]
+Temperatures=[1750,2000,2500,3000,3500]
 cut_off=1.75
-
-V=8.82
-T=3000
 
 
 folder_base = string( "/media/mathieu/Elements/CO2/" )
-folder_target = string( folder_base, V, "/", T, "K/" )
-
-file_traj = string( folder_target, "TRAJEC_fdb_wrapped.xyz" )
-
-traj = filexyz.readFileAtomList( file_traj )
-cell = cell_mod.Cell_param(V,V,V)
-cut_off=1.75
-
-
-nb_atoms = size(traj[1].names)[1]
-nb_step = size(traj)[1]
-
-folder_target_mol = string( folder_target, "Data/Molecules/" )
-
-if ! isdir( folder_target_mol )
-    Base.Filesystem.mkdir( folder_target_mol )
+folder_data_gen = string( folder_base, "/Data/Molecules/")
+folder_in_pressure = string( folder_base, "Data/Pressure/" )
+if  !isdir( folder_data_gen)
+    Base.Filesystem.mkdir(folder_data_gen)
 end
-
-handle_mol_inf = Vector{ IO }( undef, nb_atoms )
-handle_mol_fin = Vector{ IO }( undef, nb_atoms )
-for atom=1:nb_atoms
-  handle_mol_inf[atom]= open( string( folder_target_mol, "mol_inf_",atom,".xyz"), "w" )
-  handle_mol_fin[atom]= open( string( folder_target_mol, "mol_fin_",atom,".xyz"), "w" )
-end
-# handle_mol_inf_link = open( string( folder_target_mol, "mol_inf_link.xyz" ) , "w" )
-
-nb_step  = size( traj )[1]
-nb_atoms = size( traj[1].names )[1]
-
-hist_fin=zeros( Int, nb_step, nb_atoms )
-hist_inf=zeros( Int, nb_step, nb_atoms )
-hist_gen=zeros( Int, nb_step, nb_atoms )
-
-for step=1:nb_step
-
-    print("Progress: ",round(step/nb_step*100,digits=3),"%\n")
-
-    # Computing the molecules through graph exploration
-    positions_local=copy(traj[step].positions)
-    matrix = contact_matrix.buildMatrix( traj[step], cell, cut_off )
-    molecules=graph.getGroupsFromMatrix(matrix)
-    nb_molecules=size(molecules)[1]
-    matrices=graph.extractAllMatrixForTrees( matrix, molecules )
-
-    # Loop over mmolecules
-    for molecule=1:nb_molecules
-        size_molecule = size( molecules[molecule] )[1]
-        # Molecule is an atom, discard
-        if size_molecule <= 1
+handle_inf = open( string(folder_data_gen,"molecules_inf_map.dat"),"w")
+for T in Temperatures
+    print("T: ",T,"\n")
+    pressure, delta_press, volumes = readPressure( string(folder_in_pressure,T,"_P_global.dat"))
+    for V in Volumes
+        folder_target = string( folder_base, V, "/", T, "K/" )
+        file_traj = string( folder_target, "TRAJEC_fdb_wrapped.xyz" )
+        traj = filexyz.readFileAtomList( file_traj )
+        if traj == false
             continue
         end
-        # Check if molecule is finite
-        visited=zeros(Int,size(molecules[molecule]))
-        adjacent_molecule=getAllAdjacentVertex(matrices[molecule])
-        #isinf, isok = cell_mod.isInfiniteChain( visited, matrices[molecule], adjacent_molecule, positions_local, cell, 1, molecules[molecule], cut_off )
-        isinf = cell_mod.checkInfiniteChain( matrices[molecule], positions_local, cell, molecules[molecule], cut_off )
-        # If something went wrong, moving on...
-        # if ! isok
-        #     print( "Issue reconstructing molecule ", molecule, " at step: ", step, "\n" )
-        #     continue
-        # end
-        # Whether infinite or finite, the size of the molecule is counted, and it is printed in a specific file
-        if isinf
-            writeMolecule( handle_mol_inf[size_molecule], positions_local[molecules[molecule],:], traj[step].names[ molecules[molecule] ], step, molecule ) # write molecule
-            # If the molecule is infinite, we print the atoms that should be bonded but aren't.
-            #list = cell_mod.findUnlinked( matrices[molecule], positions_local[molecules[molecule],:], cell, molecules[molecule], cut_off )
-            # We put the positions of those atoms in a specific file, for visualization purposes (with VMD)
-            #writeMolecule( handle_mol_inf_link, positions_local[unique(list),:], traj[step].names[ molecules[unique(list)] ], step, molecule ) # write links
-            # Counting sizes
-            hist_inf[ step, size_molecule ] += 1
-        else
-            writeMolecule( handle_mol_fin[size_molecule], positions_local[molecules[molecule],:], traj[step].names[ molecules[molecule] ], step, molecule ) # write molecule
-            # Counting sizes
-            hist_fin[ step, size_molecule ] += 1
+        cell = cell_mod.Cell_param(V,V,V)
+        nb_atoms = size(traj[1].names)[1]
+        nb_step = size(traj)[1]
+        folder_target_mol = string( folder_target, "Data/Molecules/" )
+        if ! isdir( folder_target_mol )
+            Base.Filesystem.mkdir( folder_target_mol )
         end
-        hist_gen[ step, size_molecule ] += 1
-        # NB: here we only count the molecule by size, however that isn't fair, as a large molecule may take the
-        # totality of the atoms. Therefore, a corrective factor will be needed when computing fair statistics.
+        #
+        handle_mol_inf = Vector{ IO }( undef, nb_atoms )
+        handle_mol_fin = Vector{ IO }( undef, nb_atoms )
+        for atom=1:nb_atoms
+            handle_mol_inf[atom]= open( string( folder_target_mol, "mol_inf_",atom,".xyz"), "w" )
+            handle_mol_fin[atom]= open( string( folder_target_mol, "mol_fin_",atom,".xyz"), "w" )
+        end
+        #
+        nb_step  = size( traj )[1]
+        nb_atoms = size( traj[1].names )[1]
+        # histograms of sizes
+        hist_fin=zeros( Int, nb_step, nb_atoms )
+        hist_inf=zeros( Int, nb_step, nb_atoms )
+        hist_gen=zeros( Int, nb_step, nb_atoms )
+        # Analyzing molecules
+        for step=1:nb_step
 
+            # Computing the molecules through graph exploration
+            positions_local=copy(traj[step].positions)
+            matrix = contact_matrix.buildMatrix( traj[step], cell, cut_off )
+            molecules=graph.getGroupsFromMatrix(matrix)
+            nb_molecules=size(molecules)[1]
+            matrices=graph.extractAllMatrixForTrees( matrix, molecules )
+
+            # Loop over mmolecules
+            for molecule=1:nb_molecules
+                size_molecule = size( molecules[molecule] )[1]
+                # Molecule is an atom, discard
+                if size_molecule <= 1
+                    continue
+                end
+                # Check if molecule is finite
+                visited=zeros(Int,size(molecules[molecule]))
+                adjacent_molecule=getAllAdjacentVertex(matrices[molecule])
+                #isinf, isok = cell_mod.isInfiniteChain( visited, matrices[molecule], adjacent_molecule, positions_local, cell, 1, molecules[molecule], cut_off )
+                isinf = cell_mod.checkInfiniteChain( matrices[molecule], positions_local, cell, molecules[molecule], cut_off )
+                # Whether infinite or finite, the size of the molecule is counted, and it is printed in a specific file
+                if isinf
+                    writeMolecule( handle_mol_inf[size_molecule], positions_local[molecules[molecule],:], traj[step].names[ molecules[molecule] ], step, molecule ) # write molecule
+                    hist_inf[ step, size_molecule ] += 1
+                else
+                    writeMolecule( handle_mol_fin[size_molecule], positions_local[molecules[molecule],:], traj[step].names[ molecules[molecule] ], step, molecule ) # write molecule
+                    hist_fin[ step, size_molecule ] += 1
+                end
+                hist_gen[ step, size_molecule ] += 1
+                # NB: here we only count the molecule by size, however that isn't fair, as a large molecule may take the
+                # totality of the atoms. Therefore, a corrective factor will be needed when computing fair statistics.
+            end
+        end
+        # Removing useless files
+        for atom=1:nb_atoms
+            close( handle_mol_inf[atom] )
+            close( handle_mol_fin[atom] )
+            if sum(hist_fin[ :,atom ]) == 0
+                Base.Filesystem.rm( string( folder_target_mol, "mol_fin_",atom,".xyz") )
+            end
+            if sum(hist_inf[ :, atom ]) == 0
+                Base.Filesystem.rm( string( folder_target_mol, "mol_inf_",atom,".xyz") )
+            end
+        end
+        # Writting The dsitrbution of size at each step
+        writeSizeHistogramTime( string( folder_target_mol, "hist_inf.dat"), hist_inf )
+        writeSizeHistogramTime( string( folder_target_mol, "hist_fin.dat"), hist_fin )
+        writeSizeHistogramTime( string( folder_target_mol, "hist_gen.dat"), hist_gen )
+        # Writting the global size over all steps
+        writeSizeHistogramGlobal( string( folder_target_mol, "hist_inf.dat"), hist_inf )
+        writeSizeHistogramGlobal( string( folder_target_mol, "hist_fin.dat"), hist_fin )
+        writeSizeHistogramGlobal( string( folder_target_mol, "hist_gen.dat"), hist_gen )
+        # Computing Pressure
+        press = pressure[ volumes.== V ][1]
+        if sum(hist_inf[:,:]) > 0
+            Base.write( handle_inf, string(press," ",V," ",T," ",1,"\n") )
+        else
+            Base.write( handle_inf, string(press," ",V," ",T," ",0,"\n") )
+        end
     end
+    write(handle_inf,string("\n"))
 end
-
-for atom=1:nb_atoms
-    close( handle_mol_inf[atom] )
-    close( handle_mol_fin[atom] )
-    if hist_fin[ atom ] == 0
-        Base.Filesystem.rm( string( folder_target_mol, "mol_fin_",atom,".xyz") )
-    end
-    if hist_inf[ atom ] == 0
-        Base.Filesystem.rm( string( folder_target_mol, "mol_inf_",atom,".xyz") )
-    end
-end
-
-writeSizeHistogramTime( string( folder_target_mol, "hist_inf.dat"), hist_inf )
-writeSizeHistogramTime( string( folder_target_mol, "hist_fin.dat"), hist_fin )
-writeSizeHistogramTime( string( folder_target_mol, "hist_gen.dat"), hist_gen )
-
-writeSizeHistogramGlobal( string( folder_target_mol, "hist_inf.dat"), hist_inf )
-writeSizeHistogramGlobal( string( folder_target_mol, "hist_fin.dat"), hist_fin )
-writeSizeHistogramGlobal( string( folder_target_mol, "hist_gen.dat"), hist_gen )
+close(handle_inf)
