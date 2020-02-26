76159f5ff2d9cf2582800b3413a27f23a7c709cf
Mathieu Moog <moog.mathieu@gmail.com>
2020-02-26 16:25:53 +0100

    new file:   lengthMolecules.jl 	new file:   moleculeAnalysis.jl

---
+++ Projects/Moog_2016-2019/CO2/CO2_Liquid/Structure/molecules/lengthMolecules.jl
@@ -0,0 +1,52 @@
+# Loading necessary stuff
+using atom_mod
+using cell_mod
+using cube_mod
+using clustering
+using contact_matrix
+using filexyz
+using graph
+using exp_data
+using geom
+
+
+
+V=8.82
+T=3000
+
+folder_base=string("/media/mathieu/Elements/CO2/",V,"/",T,"K/")
+
+file_traj = string(folder_base,"TRAJEC_fdb_wrapped.xyz")
+
+traj = filexyz.readFileAtomList( file_traj )
+cell = cell_mod.Cell_param(V,V,V)
+cut_off=1.75
+
+nb_step=size(traj)[1]
+
+for step=1:nb_step
+
+    positions_local=copy(traj[step].positions)
+    matrix = contact_matrix.buildMatrix( traj[step], cell, cut_off )
+    molecules=graph.getGroupsFromMatrix(matrix)
+    nb_molecules=size(molecules)[1]
+    matrices=graph.extractAllMatrixForTrees( matrix, molecules )
+
+    nb_atoms=size(traj[1].names)[1]
+    visited=zeros(Int,nb_atoms)
+
+    for molecule=1:nb_molecules
+        if size(molecules[molecule])[1] <= 1
+            continue
+        end
+        adjacent_molecule=getAllAdjacentVertex(matrices[molecule])
+        visited=zeros(Int,size(molecules[molecule]))
+        cell_mod.unWrapOrthoOnce( visited, matrices[molecule], adjacent_molecule, positions_local, cell, 1, molecules[molecule] )
+    end
+
+    traj[step].positions=positions_local
+end
+
+folder_out=string(folder_base,"/Data/Molecules/")
+file_out=string(folder_out,"all_unwrap.xyz")
+filexyz.writeXYZ(file_out,traj)
