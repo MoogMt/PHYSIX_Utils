66d7938b2ccc08a148e56e1633ae5ef8e8c355ff
Mathieu Moog <moog.mathieu@gmail.com>
2020-02-27 21:21:56 +0100

    modified:   ../Exp/gr_fq.jl

--- Projects/Moog_2016-2019/CO2/CO2_Liquid/Exp/gr_fq.jl
+++ Projects/Moog_2016-2019/CO2/CO2_Liquid/Exp/gr_fq.jl
@@ -13,10 +13,11 @@ using exp_data
 
 # Folder for data
 #folder_base="/media/moogmt/Stock/Mathieu/CO2/AIMD/Liquid/PBE-MT/"
-folder_base="/home/moogmt/Data/CO2/CO2_AIMD/"
+#folder_base = "/home/moogmt/Data/CO2/CO2_AIMD/"
+folder_base = "/media/mathieu/Elements/CO2/"
 
-Temperatures=[2500]
-Volumes=[8.82]
+Temperatures = [ 1750, 2000, 2500, 3000 ]
+Volumes = [ 10.0, 9.8, 9.5, 9.4, 9.375, 9.35, 9.325, 9.3, 9.25, 9.2, 9.15, 9.1, 9.05, 9.0, 8.82, 8.8, 8.6]
 
 rmin=0
 dr=0.001
@@ -27,8 +28,17 @@ for V in Volumes
         print("V: ",V," T:",T,"K\n")
 
         folder_in=string(folder_base,V,"/",T,"K/")
-        file_in=string(folder_in,"TRAJEC_wrapped.xyz")
-        folder_out=string(folder_in,"Data/")
+        file_in=string(folder_in,"TRAJEC_fdb_wrapped.xyz")
+
+        if ! isfile( file_in )
+            continue
+        end
+
+        folder_out=string(folder_in,"Data/Exp/")
+
+        if !isdir( folder_out )
+            Base.Filesystem.mkdir( folder_out )
+        end
 
         file_out_gr=string(folder_out,"gr.dat")
 
