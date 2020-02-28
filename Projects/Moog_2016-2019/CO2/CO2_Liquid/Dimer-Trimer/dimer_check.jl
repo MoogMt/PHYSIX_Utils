6cd8417c441092efbf0d34801670bc968b3099d7
Mathieu Moog <moog.mathieu@gmail.com>
2020-02-28 15:02:29 +0100

    modified:   ../Dimer-Trimer/dimer_check.jl 	deleted:    ../Dimer-Trimer/dimer_detect.jl 	deleted:    ../Dimer-Trimer/exchange_compute.jl

--- Projects/Moog_2016-2019/CO2/CO2_Liquid/Dimer-Trimer/dimer_check.jl
+++ Projects/Moog_2016-2019/CO2/CO2_Liquid/Dimer-Trimer/dimer_check.jl
@@ -1,55 +1,73 @@
 # Loading file
-include("contactmatrix.jl")
+using atom_mod
+using cell_mod
+using cube_mod
+using clustering
+using filexyz
+using pdb
+using contact_matrix
 
 nbC=32
 nbO=64
 
+folder_base = string( "/media/mathieu/Elements/CO2/" )
+
 cut_off=1.75
 
-Volumes=[9.325]
-Temperatures=[2000,2500,3000]
+Volumes      = [ 10.0, 9.8, 9.5, 9.4, 9.375, 9.35, 9.325, 9.3, 9.25, 9.2, 9.15, 9.1, 9.05, 9.0, 8.82, 8.8, 8.6 ]
+Temperatures = [ 1750, 2000, 2500, 3000 ]
 
 for T in Temperatures
     for V in Volumes
-
-        folder=string("/media/moogmt/Stock/CO2/AIMD/Liquid/PBE-MT/",V,"/",T,"K/")
-        folder_out=string("/media/moogmt/Stock/CO2/AIMD/Liquid/PBE-MT/",V,"/",T,"K/Data/")
-        file=string(folder,"TRAJEC_wrapped.xyz")
-
-        if isfile( string(folder,"TRAJEC_wrapped.xyz") )
-            traj = filexyz.readFastFile(file)
-            cell=cell_mod.Cell_param(V,V,V)
-            nb_steps=size(traj)[1]
-            nb_atoms=size(traj[1].names)[1]
-            dimer_out=open(string(folder_out,"dimer-",cut_off,".dat"),"w")
-            for step=1:nb_steps
-                print("Volume :",V," Temperature: ",T,"K Progress: ",step/nb_steps*100,"% \n")
-                bonds=zeros(nbC,nbO)
-                for carbon=1:nbC
-                    for oxygen=1:nbO
-                        if  cell_mod.distance( traj[step], cell, carbon, oxygen+nbC ) < cut_off
-                            bonds[carbon,oxygen]=1
-                        end
-                    end
+        folder_target = string( folder_base, V, "/", T, "K/" )
+        file_traj = string( folder_target, "TRAJEC_fdb_wrapped.xyz" )
+        if ! isfile( file_traj )
+            continue
+        end
+        folder_out = string( folder_target, "Data/Dimers/" )
+        if ! isdir( folder_out )
+            Base.Filesystem.mkdir( folder_out )
+        end
+        traj = filexyz.readFileAtomList( file_traj )
+        cell = cell_mod.Cell_param( V, V, V )
+        nb_steps = size( traj )[1]
+        nb_atoms = size( traj[1].names )[1]
+        dimer_out = open( string( folder_out, "dimer-", cut_off, ".dat" ), "w" )
+        for step=1:nb_steps
+            print( "Volume :", V, " Temperature: ", T, "K Progress: ", round(step/nb_steps*100, digits=3 ), "% \n" )
+            cm = contact_matrix.buildMatrix( traj[step], cell, cut_off )
+            used = zeros( nb_atoms )
+            for carbon1=1:nbC-1
+                if used[ carbon1 ] == 1
+                    continue
                 end
-                used=zeros(nbC)
-                for carbon1=1:nbC-1
-                    for carbon2=carbon1+1:nbC
-                        for oxygen1=1:nbO-1
-                            if bonds[carbon1,oxygen1] > 0  && bonds[carbon2,oxygen1] > 0
-                                for oxygen2=oxygen1+1:nbO
-                                    if bonds[carbon1,oxygen2] > 0  && bonds[carbon2,oxygen2] > 0
-                                        write(dimer_out,string(step," ",carbon1," ",carbon2," ",oxygen1," ",oxygen2,"\n"))
-                                        break
-                                    end
+                for carbon2=carbon1+1:nbC
+                    if used[ carbon2 ] == 1
+                        continue
+                    end
+                    for oxygen1=1:nbO
+                        if used[ oxygen1 ] == 1
+                            continue
+                        end
+                        if cm[ carbon1, nbC+oxygen1 ] > 0  && cm[ carbon2, nbC+oxygen1 ] > 0
+                            for oxygen2=oxygen1+1:nbO
+                                if used[ oxygen2 ] == 1
+                                    continue
+                                end
+                                if cm[ carbon1, oxygen2 ] > 0  && bonds[ carbon2, nbC+oxygen2 ] > 0
+                                    write( dimer_out, string( step, " ", carbon1, " ", carbon2," ", nbC+oxygen1, " ", nbC+oxygen2, "\n" ) )
+                                    used[ carbon1 ] = 1
+                                    used[ carbon2 ] = 1
+                                    used[ nbC + oxygen1 ] = 1
+                                    used[ nbC + oxygen2 ] = 1
+                                    break
                                 end
                             end
                         end
                     end
                 end
             end
-            close(dimer_out)
         end
-
+        close( dimer_out )
     end
 end
