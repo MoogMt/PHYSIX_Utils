1d323bb90be5b46a42d4826acf3b5acfefa04d2d
Mathieu Moog <moog.mathieu@gmail.com>
2020-02-28 14:07:08 +0100

    modified:   ../Exp/diff_indiv.jl

--- Projects/Moog_2016-2019/CO2/CO2_Liquid/Exp/diff_indiv.jl
+++ Projects/Moog_2016-2019/CO2/CO2_Liquid/Exp/diff_indiv.jl
@@ -1,6 +1,3 @@
-GPfolder=string("/home/moogmt/PHYSIX_Utils/GPlib/Julia/")
-push!(LOAD_PATH, GPfolder)
-
 using atom_mod
 using cell_mod
 using cube_mod
@@ -73,17 +70,14 @@ function computingMSD( V::T1, T::T2, file_traj::T3, file_out::T4, names::Vector{
     return msd
 end
 
-# Model for a linear fit
-@. model(x, p) = p[1]*x
-
 # Folder for data
 folder_base="/media/moogmt/Stock/Mathieu/CO2/AIMD/Liquid/PBE-MT/"
 folder_base="/home/moogmt/Data/CO2/CO2_AIMD/"
 folder_base="/media/mathieu/Elements/CO2/"
 
 
-Volumes=[9.8]
-Temperatures=[3000]
+Volumes      = [ 10.0, 9.8, 9.5, 9.4, 9.375, 9.35, 9.325, 9.3, 9.25, 9.2, 9.15, 9.1, 9.05, 9.0, 8.82, 8.8, 8.6 ]
+Temperatures = [ 1750, 2000, 2500, 3000 ]
 
 for V in Volumes
     for T in Temperatures
@@ -92,17 +86,21 @@ for V in Volumes
         if ! isfile(file_traj)
             continue
         end
+        print("V: ",V," T: ",T," K\n")
         folder_out=string(folder_target,"Data/Exp/")
         if ! isdir( folder_out )
             Base.Filesystem.mkdir( folder_out )
         end
         file_out = string(folder_out,"MSD.dat")
-        msd,test = computingMSD( V, T, file_traj, file_out )
+        msd = computingMSD( V, T, file_traj, file_out )
         file_outC = string( folder_out, "MSD_C.dat")
-        msd_C,test=computingMSD( V, T, file_traj, file_outC, ["C"], [6] )
+        msd_C =computingMSD( V, T, file_traj, file_outC, ["C"], [6] )
         file_outO = string( folder_out, "MSD_O.dat" )
-        msd_O,test=computingMSD( V, T, file_traj, file_outO, ["O"], [8] )
-        msd_CO = msd_C - msd_O
+        msd_O =computingMSD( V, T, file_traj, file_outO, ["O"], [8] )
+        if size( msd_C )[1] == ()
+            continue
+        end
+        msd_CO = msd_C-msd_O
         file_o = open( string(folder_out,"MSD_CO.dat"), "w" )
         for i=1:size( msd_C )[1]
             Base.write( file_o, string( i, " ", msd_CO[i], "\n" ) )
@@ -110,31 +108,3 @@ for V in Volumes
         close( file_o )
     end
 end
-
-# function blockAverage( data::Vector{T1}, max_block_size::T2 ) where { T1 <: Real, T2 <: Int }
-#     size_record=[]
-#     sigma_record=[]
-#     for size_block=2:1:max_block_size
-#         nb_block=Int(trunc(size(data)[1]/size_block))
-#         meta_avg=0
-#         avg_all_blocks=0
-#         count_=1
-#         for block=1:nb_block
-#             avg_block=0
-#             var_block=0
-#             for i=1:size_block
-#                 avg_block += data[count_]
-#                 var_block += data[count_]*data[count_]
-#                 count_ += 1
-#             end
-#             avg_block=avg_block/size_block
-#             meta_avg+=sqrt(var_block/size_block - avg_block*avg_block)
-#             avg_all_blocks = avg_all_blocks + avg_block
-#         end
-#         avg_all_blocks = avg_all_blocks/nb_block
-#         sigma = sqrt( meta_avg/nb_block )
-#         push!(size_record,size_block)
-#         push!(sigma_record,sigma)
-#     end
-#     return size_record,sigma_record
-# end
