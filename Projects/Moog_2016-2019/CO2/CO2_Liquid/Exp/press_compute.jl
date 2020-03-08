3d7ca4109f7b9a20a089adabe531a3ef51ca3296
MoogMt <moog.mathieu@gmail.com>
2020-03-09 00:33:00 +0100

    modified:   Projects/Moog_2016-2019/CO2/CO2_Liquid/Dimer-Trimer/exchange.jl 	modified:   Projects/Moog_2016-2019/CO2/CO2_Liquid/Exp/press_compute.jl

--- Projects/Moog_2016-2019/CO2/CO2_Liquid/Exp/press_compute.jl
+++ Projects/Moog_2016-2019/CO2/CO2_Liquid/Exp/press_compute.jl
@@ -26,6 +26,38 @@ for i=1:2
 end
 total_mass *= conversion.amu2gram
 
+# for T in Temperatures
+#     folder_out_general = string( folder_base, "Data/Pressure/" )
+#     if !isdir( folder_out_general )
+#         Base.Filesystem.mkdir(folder_out_general )
+#     end
+#     file_out_general = string( folder_out_general,T,"_P_global.dat")
+#     handle_out_gen = open( file_out_general, "w" )
+#     for V in Volumes
+#         folder_target=string(folder_base, V, "/",T,"K/")
+#         file_target = string(folder_target,"Pressure_fdb")
+#         if ! isfile(file_target)
+#             continue
+#         end
+#         pressure = press_stress.readPressure( file_target )
+#         folder_out = string( folder_target, "Data/Pressure/")
+#         if !isdir( folder_out )
+#             Base.Filesystem.mkdir(folder_out)
+#         end
+#         file_out_loc = string( folder_out, "P_global.dat" )
+#         handle_out_loc = open( file_out_loc, "w")
+#         Base.write( handle_out_loc, string( Statistics.mean(pressure)," ",Statistics.std(pressure),"\n"))
+#         Base.write( handle_out_gen, string( V," ", V*V*V/(nb_atoms), " ", Statistics.mean(pressure)," ",Statistics.std(pressure)," ",total_mass/(V*V*V*conversion.a3tocm3),"\n"))
+#         close( handle_out_loc )
+#     end
+#     close( handle_out_gen )
+# end
+
+frac = 0.05
+max_size = 20000
+block_size = round(Int, frac*max_size)
+n_block = 20
+
 for T in Temperatures
     folder_out_general = string( folder_base, "Data/Pressure/" )
     if !isdir( folder_out_general )
@@ -46,8 +78,14 @@ for T in Temperatures
         end
         file_out_loc = string( folder_out, "P_global.dat" )
         handle_out_loc = open( file_out_loc, "w")
-        Base.write( handle_out_loc, string( Statistics.mean(pressure)," ",Statistics.std(pressure),"\n"))
-        Base.write( handle_out_gen, string( V," ", V*V*V/(nb_atoms), " ", Statistics.mean(pressure)," ",Statistics.std(pressure)," ",total_mass/(V*V*V*conversion.a3tocm3),"\n"))
+        pressure_avg = Statistics.mean(pressure)
+        err = 0
+        for i_block=1:n_block
+            err += Statistics.std(pressure[ (i_block-1)*block_size:i_block*blocksize ])
+        end
+        err /= sqrt(n_block)
+        Base.write( handle_out_loc, string( pressure_avg," ",err,"\n"))
+        Base.write( handle_out_gen, string( V, " ", V*V*V/(nb_atoms), " ", pressure_avg, " ", err, " ", total_mass/(V*V*V*conversion.a3tocm3 ), "\n" ) )
         close( handle_out_loc )
     end
     close( handle_out_gen )
